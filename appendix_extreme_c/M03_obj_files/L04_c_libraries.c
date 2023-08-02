fbc9a56fd258268a864c995f13b117f12c7871c0
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-08-01 20:26:15 -0500

    Extrem C P3: Talking about dynamic libraries

--- appendix_extreme_c/M03_obj_files/L04_c_libraries.c
+++ appendix_extreme_c/M03_obj_files/L04_c_libraries.c
@@ -30,10 +30,32 @@
  * 
  * The flags come from c (create the archive), r (insert file members) and s (write obj file index).
  * 
+ * DYNAMIC LIBRARY: Also known as shared libraries, they are not part of the final executable. So, the
+ * linker detects the undefined symbols and search for them so it can put them in the final output. In these
+ * cases, the linker can be interpreted as a loader, and it should search for them in .so files. 
  * 
+ * For creating shared object file you can use:
  * 
+ *     gcc -c <file.c> -fPIC -o <file.o> 
  * 
+ * The flag added means Position Independent Code, which says they have relative adresses and can have
+ * different addresses in different process. Here the loader creates memory maps to work with them.
  * 
+ * To create a shared file, the commands are:
+ *    gcc -shared <files.o> ... -o <lib_name>.so
+ *    mkdir -p /opt/<library>
+ *    mv <lib_name>.so /opt/<library>
+ * 
+ * Also, after you compile a file with this type of library, you will need to embed the exectuable.
+ *   
+ *   gcc <main.o> -L/opt/<library> -l<lib-name> -lm -o <output.out>
+ *   export LD_LIBRARY_PAHT=/opt/<library>  # Option 1
+ *   LD_LIBRARY_PATH=/opt/<library> ./<output.out> # Option 2
+ * 
+ * Another possibility for interacting with dynamic libraries are the commands:
+ *  - dlopen (<PATH>, <OPT>); -> Open dynamic library, one common OPT ist 'RTLD_LAZY'
+ *  - dlsym (handle, <function_in_lib>); -> Return function pointer which can be invoked.
+ *
  * NOTE: A linker uses the library to try to extract the relocatable object file, then it will look
  * for missing parts and try to resolve them.
  */
@@ -41,7 +63,9 @@
 int main(int argc, char const **argv)
 {
     printf("Do not forget to read the comments to know more about libraries...\n");
-    printf("Useful resources:\n");
-    printf("- https://stackoverflow.com/questions/29714300/what-does-the-rcs-option-in-ar-do\n");
+    printf("Useful resources for static libraries:\n");
+    printf("\t- https://stackoverflow.com/questions/29714300/what-does-the-rcs-option-in-ar-do\n");
+    printf("Useful resources for dynamic libraries:\n");
+    printf("\t- https://www.technovelty.org/linux/plt-and-got-the-key-to-code-sharing-and-dynamic-libraries.html\n");
     return 0;
 }
