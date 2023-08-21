b09c9dd1a4a4d2aaf78090d44d78d378bff140ad
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-08-20 22:30:34 -0500

    Extrem C P6: About domain, instances and construction in OOP

--- appendix_extreme_c/M06_OOP_encapsulation/L01_OOP_thinking.c
+++ appendix_extreme_c/M06_OOP_encapsulation/L01_OOP_thinking.c
@@ -18,6 +18,35 @@
  * A solution to a problem is made based on an explanation in terms of the concepts that conform/create
  * the problem (and its relationship). In the case of a computer, you are the one who solve the problem
  * breaking it in single parts, and the computer is your tool for executing the solution.
+ * 
+ * Each object has attributes, it can be color, size, status... and go on with the ideas that defines
+ * the object you are considering. But keep in mind it can change, you can paint something to change its
+ * color, or the size can change through the time, it means they are 'mutable'. Also, they can be 
+ * 'stateless' beacuse they do not carry states, for example, if the concept cannot change, because the 
+ * darkness will always be the same.
+ * 
+ * The objects we are considering are present in a well-defined domain, the boundaries of the system where 
+ * it works, and they are related with a general concept that relates the objects present in the domain. It
+ * also relates with the programming language, as it would be difficult to use if the domain
+ * isn't compatible with the desired objective. Inside this domains, there are relationships, which can
+ * complement the definitions and the models of the object, which derivates the relationships are born with 
+ * the addition of common attributes which can be 'mutable' and 'immutable'.
+ * 
+ * To have an object we need to construct it (no create or build the object):
+ *  - It can be an empty object (without attributes or minimum attributes).
+ *  - Attributes are added in the run time according to the inner structure and the surrounding environment.
+ *  - Each object is a different entity, even though, they have the same information.
+ *  - In interpreted programming languages the object is kept as a map/hash that can be modified during
+ *    the execution, it also known as 'prototype-based OOP'. (It happens with Ruby, Python, PHP)
+ *  - Another approach is to create immutable objects since the beginning, as the structure will be kept. This
+ *    is known as predesiged 'object template' or 'class template', which will be the base for the OOP. (Present
+ *    int Java, C++) , but it real name is 'class-based OOP'.
+ * 
+ * Now, time for clarification an object and an instance are the same thing, but it doesn't mean the same with 
+ * reference. This last one means that a pointer has the memory adress of the object. And here, in C, the objects
+ * doesn't have name but the reference has one, do you remember about Stack and Heap? (Including its differences
+ * with the memory allocation and managment".
+ * 
 */
 
 #include <string.h>
