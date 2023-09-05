d7ef12f09bfdc51845c74349aa1a08e11bbfdaac
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-09-05 12:09:42 -0500

    Extrem CP8: Example of private approach of inheritance P1

---
+++ appendix_extreme_c/M08_inheritance/L02_inheritance_type1.c
@@ -0,0 +1,10 @@
+// BASED ON THE "EXTREM C BOOK - 1 EDITION"
+// Code was tested with gcc
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    
+    return 0;
+}
---
+++ appendix_extreme_c/M08_inheritance/L02_type1_animal_p.h
@@ -0,0 +1,14 @@
+// BASED ON THE "EXTREM C BOOK - 1 EDITION"
+// Code was tested with gcc
+
+#ifndef ANIMAL_T1_P_H
+#define ANIMAL_T1_P_H
+
+// Private definition
+typedef struct
+{
+    char species[20];
+    char diet[10];
+} animal_t;
+
+#endif
