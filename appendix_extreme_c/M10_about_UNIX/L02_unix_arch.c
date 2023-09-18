23c975f1da31c865b1c618261cb1242fdef78326
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-09-18 18:48:50 -0500

    Extrem C P10: CLI, GUI and SUS

--- appendix_extreme_c/M10_about_UNIX/L02_unix_arch.c
+++ appendix_extreme_c/M10_about_UNIX/L02_unix_arch.c
@@ -25,6 +25,28 @@
  * - SIMPLE UNIX SPECIFICATION: It is the close defined interface for the programmer.
  * - USER APPLICATION: The apps of the OS that are related with the user and what he wants to do, like web browsers, editors,
  *   among others.
+ * 
+ * They key for interaction is a 'human interface' that can be a terminal (CLI, Command Line Interface) or a GUI (Graphical User Interface),
+ *  they allow the control of the hardware by using the shell ring, so there must be an API (Application Programming Interface) provided.
+ * 
+ * Think an API as a group of header files with declarations that can be used, so there is a standard way to
+ * use them in Unix system but each Unix-compliant system can has some differences in its static and dynamic libraries. As a
+ * standard, Unix has this UNIX compliant systems and UNIX-like systems (Linux).
+ * 
+ * CURIOSITY: printf is not part of C, it is part of the SUS standard contained in the C standard library.
+ * 
+ * The SUS standart (V4), has a list of APIs related to:
+ * - System Interfaces: Function that should be used in any C program.
+ * - Header Interfaces: List of campatibles systems, some of them are stdio.h and stlib.h.
+ * - Utility interface: For utilitys and command line programs (your terminal toolkit).
+ * - Scripting interface: For shell scripting and task automation.
+ * - XCURSES interfaces: For interaction between C and minimalist GUI.
+ * 
+ * But... what about filesystems and the places where those headers are found? For that exits a convention that is:
+ * "All headers must be found in /usr/include or /usr/local/include".
+ * 
+ * And for the usage, exist the C Standar Library or libc that is a set of functions together with 
+ * the implementation of the exposed functions (by respecting SUS).
 */
 
 int main(int argc, char** argv)
@@ -33,6 +55,7 @@ int main(int argc, char** argv)
     printf("\thttps://kukuruku.co/post/the-collapse-of-the-unix-philosophy/\n");
     printf("\thttps://www.linfo.org/unix_philosophy.html\n");
     printf("\thttps://en.wikipedia.org/wiki/Unix_philosophy\n");
+    printf("\thttp://www.unix.org/version4/GS5_APIs.pdf\n");
 
     return 0;
 }
