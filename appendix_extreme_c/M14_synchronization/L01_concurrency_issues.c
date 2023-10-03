3a85019fe9b7e463f42a3a71aeb893a65be6c2cf
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-10-03 11:45:11 -0500

    M14: About some concurrency issues

---
+++ appendix_extreme_c/M14_synchronization/L01_concurrency_issues.c
@@ -0,0 +1,27 @@
+// BASED ON THE "EXTREM C BOOK - 1 EDITION"
+// Code was tested with gcc
+
+#include <stdio.h>
+
+/**
+ * Exist the probability that concurrency fails or its enough, even fixing them create problems, as adding
+ * code make it different to its original root. Another problem is the 'data race' as the shared
+ * resources  and need to improve mangament there.  And as you modify some task may not get the chance
+ * to run and become 'starved'.
+ * 
+ * We call these problems:
+ *  - INTRINSIC CONCURRENCY ISSUES: Due to lack of control mechanism.
+ *    It is based on the concept that always a interleaving can happen, and it becomes and issue when some 
+ *    constraints are not kept. This will lead to 'race condition' systems which means that the constraints
+ *    can be missed at some point. 
+ *    The real problem here is that it can generate data corruptions and vulnerate the 'data integrity'.
+ *  - Post Synscorization issues: Attempt of resolution from the previous one.
+*/
+
+int main(int argc, char* *argv[])
+{
+    // Pseudo code for crash generation with interleaving in C
+
+    // M
+    return 0;
+}
