b7f28d47aba46de99e405241f5f08b7c3ca5500d
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-10-09 14:28:58 -0500

    Extrem C 14: Post sunchonization with sleep-notify and semaphores

--- appendix_extreme_c/M14_synchronization/L03_synchronization_tec.c
+++ appendix_extreme_c/M14_synchronization/L03_synchronization_tec.c
@@ -8,12 +8,21 @@
  * so there are 'happens-before' constraints that keep the order of execution between tasks.
  * 
  * - BUSY-WAITS AND SPIN LOCKS: It is a complex form of saying that a task needs to wait after another task in order
- *   to begin. 
+ *   to begin. But this isn't optimized, as it implies a wait time that could be used in another task.
+ * 
+ * - SLEEP/NOTIFY MECHANISM: It notifies a task when its state is completed, and it is considered base in many OS.
+ * 
+ * - SEMAPHORES AND MUTEX: The first concept was an invention of Edsger Dijkstra while he develop "THE Multiprogramming
+ *   System", and its refered to using simple variables to sunchronize access to a shared resource. They work with queue
+ *   lists and they are a line for a 'critical section'. The second concept refers to mutual exclusion, as only one can 
+ *   enter to the critical section, no deadlock should exist and it should be collaborative.
+ * 
+ * - 
 */
 
 int main(int argc, char const *argv[])
 {
-    printf("Example 1: Without busy-wait mechanism:\n");
+    printf("Example 1: Without synchronization mechanism:\n");
     printf("System\n");
     printf("{\n");
     printf("    Task A\n");
@@ -44,7 +53,72 @@ int main(int argc, char const *argv[])
     printf("        printf(\"World!\");\n");
     printf("    }\n");
     printf("}\n\n");
+
+    printf("Example 3: With sleep/notify mechanism:\n");
+    printf("System\n");
+    printf("{\n");
+    printf("    Task A\n");
+    printf("    {\n");
+    printf("        printf(\"Hello!\");\n");
+    printf("        notify(B); // Invented notification with C\n");
+    printf("    }\n");
+    printf("    Task B\n");
+    printf("    {\n");
+    printf("        if(notification)\n");
+    printf("        {\n");
+    printf("            printf(\"World!\");\n");
+    printf("        }\n");
+    printf("    }\n");
+    printf("}\n\n");
+
+    printf("Example 4: With sleep/notify mechanism improved:\n");
+    printf("System\n");
+    printf("{\n");
+    printf("    Shared State\n");
+    printf("    {\n");
+    printf("        int done = 0;\n");
+    printf("    }\n");
+    printf("    Task A\n");
+    printf("    {\n");
+    printf("        done = 1;\n");
+    printf("        printf(\"Hello!\");\n");
+    printf("        notify(B); // Invented notification with C\n");
+    printf("    }\n");
+    printf("    Task B\n");
+    printf("    {\n");
+    printf("        while(!notification)\n");
+    printf("        {\n");
+    printf("            atomic_sleep(); // Another invented routine for learning purposes.\n");
+    printf("        }\n");
+    printf("        printf(\"World!\");\n");
+    printf("    }\n");
+    printf("}\n\n");
     
+    printf("Example 5: With sleep/notify mechanism improved:\n");
+    printf("System\n");
+    printf("{\n");
+    printf("    Shared State\n");
+    printf("    {\n");
+    printf("        int con = 0;\n");
+    printf("        s = semaphore(1); // Allow one task");
+    printf("    }\n");
+    printf("    Task A\n");
+    printf("    {\n");
+    printf("        enterCriticalSec(s)\n");
+    printf("        a = con;\n");
+    printf("        a = a + 1;\n");
+    printf("        con = a;\n");
+    printf("        exitCriticalSec(s)\n");
+    printf("    }\n");
+    printf("    Task B\n");
+    printf("    {\n");
+    printf("        enterCriticalSec(s)\n");
+    printf("        b = con;\n");
+    printf("        b = b + 2;\n");
+    printf("        con = b;\n");
+    printf("        exitCriticalSec(s)\n");    
+    printf("    }\n");
+    printf("}\n\n");
     
     return 0;
 }
