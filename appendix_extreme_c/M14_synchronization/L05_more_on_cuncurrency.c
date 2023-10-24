2290de429f6e57b5860abd5e1c6c3459b11c908c
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-10-23 21:12:58 -0500

    Extrem C P14: Unsaved changes from 13 October

--- appendix_extreme_c/M14_synchronization/L05_more_on_cuncurrency.c
+++ appendix_extreme_c/M14_synchronization/L05_more_on_cuncurrency.c
@@ -16,4 +16,26 @@
  * with the CPU for running processes and threads. But what are the differences bewteen these two:
  *  
  * - Processes: Isolated form one another, so they do not have access between them.
+ * - Threads: Used to bring concurrency into a process by having many of them executing intructions,
+ *   and they are local to a process.
+ * 
+ * But they are mainly refered as 'tasks' and the kernel see them as a list/queue of task waiting
+ * for some time with the CPU. This implies some strategies for scheduling a task, we have Cooperative
+ * Scheduling (Give the CPU a task and wait cooperation, leaving the core at some point) and Preemtive 
+ * Scheduling (A task has time with the CPU until the scheduler take it back, this given time is called
+ * time slice or quantum). The last one is the most used in kernels and there are many types, one of them
+ * is 'round robin' which is based in a starvation-free access to a shared resource, and it can be
+ * modified to implement priorizations.
+ * 
+ * * MULTI-PROCESSING: Using processes to do concurrent task, for example, with CGI (Common Gateway Interface)
+ *   which means that a new HTTP request launch a new 'interpreter process' so it can manage simultaneous
+ *   requestst.
+ *   These process needs to share a lot of information  to keep the cluster running and this implies
+ *   distribuited systems. And when there is a shared state the complexity increases.
+ *   Now, remember that a process has a private memory, so sharing is difficult. With this said, let's mention
+ *   some techniques:
+ * 
+ *   - FILE SYSTEM: Leave something in memory, so simple. 
+ *   - MEMORY-MAPPED FILE: Some regions cna be used by a certain number of processes to share info (read/modify).
+ *   - NEWTWORK: 
 */
