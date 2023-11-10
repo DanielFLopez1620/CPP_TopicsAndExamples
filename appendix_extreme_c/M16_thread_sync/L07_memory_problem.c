3ef2099f25f7ccc1f0e289079d1534f39846a6f3
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-11-10 10:42:19 -0500

    Extrem C P16: A memory problem example

--- appendix_extreme_c/M16_thread_sync/L07_memory_problem.c
+++ appendix_extreme_c/M16_thread_sync/L07_memory_problem.c
@@ -1,10 +1,15 @@
 // BASED ON THE "EXTREM C BOOK - 1 EDITION"
-// Code was not tested
+// Code was tested with gcc and can lead to crashes, as there are some interleavings.
 
 #include <stdio.h>
 
 /**
+ * If you run this code multiple times, there is a chance that the pointer become dangling, so there is also
+ * a chance for crashes, as there is an interleaving here.
  * 
+ * This is the case you need to analize in order to determinate what a proper sunchronization
+ * should be used. If you haven't notice, the problem of the code below is the shared resource and then
+ * the order the threads could be called.
 */
 
 #include <stdlib.h>
@@ -16,8 +21,26 @@ int* shared_resource;
 void* first_thread_content(void* arg);
 void* second_thread_content(void* arg);
 
-//--> 
+int main(int argc, char **argv)
+{
+    shared_resource = NULL;
 
+    pthread_t th1;
+    pthread_t th2;
+
+    pthread_create(&th1, NULL, first_thread_content, NULL);
+    pthread_create(&th2, NULL, second_thread_content, NULL);
+
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+    return 0;
+}
+
+/**
+ * Generic content of a thread, that saves a number by using its access to a global pointer.
+ * 
+ * @param arg Generic pointer to an argument.
+*/
 void* first_thread_content(void* arg)
 {
     int local_num = 1620;
@@ -26,6 +49,11 @@ void* first_thread_content(void* arg)
     return NULL;
 }
 
+/**
+ * Generic content of a thread, that saves prints a number by using its access to a global pointer.
+ * 
+ * @param arg Generic pointer to an argument.
+*/
 void* second_thread_content(void* arg)
 {
     printf("Num: %d\n", *shared_resource);
