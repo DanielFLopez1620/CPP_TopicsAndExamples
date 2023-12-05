3efdf378ecabb1a0598b5af1fc2a746b4565377b
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-12-05 08:43:23 -0500

    Extrem C P18: Initialization of mutex mechanism control in processes

--- appendix_extreme_c/M18_process_sync/L04_named_mutex_2.c
+++ appendix_extreme_c/M18_process_sync/L04_named_mutex_2.c
@@ -27,6 +27,8 @@ int* cancel_flag_ptr = NULL;
 int mt_shm_fd = -1;
 int mt_owner = 0;
 
+pthread_mutex_t* mt = NULL;
+
 void init_shr();
 void shutdown_resource();
 
@@ -116,6 +118,88 @@ void shutdown_resource()
         if(shm_unlink(SHM) < 0)
         {
             fprintf(stderr, "ERROR: Unlinking shared memory wasn't achieved: %s\n", strerror(errno));
+            exit(1);
+        }
+    }
+}
+
+void init_mutex();
+void init_mutex()
+{
+    // Open shared object in read/write mode to check if it exists
+    mt_shm_fd = shm_open(MT_SHM, O_RDWR, 0600);
+    if(mt_shm_fd >= 0)
+    {
+        mt_owner = 0;
+        fprintf(stdout, "Mutex shared resources are available\n");
+    }
+    // If the mutex shared space isn't available, try to create.
+    else if (errno == ENOENT)
+    {
+        fprintf(stderr, "WARN: Mutex shared resource isn't available\n");
+        fprintf(stdout, "Now, creating mutex shared resources...\n");
+        mt_shm_fd = shm_open(MT_SHM, O_CREAT | O_EXCL | O_RDWR, 0600);
+        // Check creation of shared memory
+        if(mt_shm_fd >= 0)
+        {
+            mt_owner = 1;
+            fprintf(stdout, "Mutex shared resources have been created.");
+        }
+        else
+        {
+            fprintf(stderr, "ERROR: Couldn't created shared memory for mutex: %s\n", strerror(errno));
+            exit(1);
+        }
+    }
+
+    // If this process is the owner (creator), search for truncation of the region
+    if(mt_owner)
+    {
+        if(ftruncate(mt_shm_fd, sizeof(pthread_mutex_t)) < 0)
+        {
+            fprintf(stderr, "ERROR: Couldn't truncate region for mutex: %s\n", strerror(errno));
+            exit(1);
+        }
+    }
+
+    // Map region in order to access to it, by using a generic pointer
+    void* map = mmap(0, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, mt_shm_fd, 0);
+    if(map == MAP_FAILED)
+    {
+        fprintf(stderr, "ERROR: Mapping region wasn't possible: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // Get access to the mapped region, with the created mutex by using the generic pointer
+    mt = (pthread_mutex_t*)map;
+
+    // Create the contorl mechanism (mutex, with the respective attribute)
+    if(mt_owner)
+    {
+        int status = -1;
+        pthread_mutexattr_t attr;
+        if((status = pthread_mutexattr_init(&attr)))
+        {
+            fprintf(stderr, "ERROR: Init attributes for mutex wasn't achieved: %s\n", strerror(errno));
+            exit(1);
+        }
+
+        // Set shared resource, the flag PTHREAD_PROCESS_SHARED allows usage by multiple processes.
+        if((status = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED)))
+        {
+            fprintf(stderr, "ERROR: Setting attributes of mutex wasn't possible: %s\n", strerror(errno));
+            exit(1);
+        }
+
+        if((status = pthread_mutex_init(mt, &attr)))
+        {
+            fprintf(stderr, "ERROR: Couldn't initialize mutex: %s\n", strerror(errno));
+            exit(1);
+        }
+
+        if((status = pthread_mutexattr_destroy(&attr)))
+        {
+            fprintf(stderr, "ERROR: While destruction of mutex attribute: %s\n", strerror(errno));
             exit(1);
         }
     }
