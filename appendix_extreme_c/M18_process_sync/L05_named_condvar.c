88f4edc81080a1e7458436f099ad23f0c8052c48
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-12-17 11:39:53 -0500

    Extrem C P18: Documenting class for cv and starting main

--- appendix_extreme_c/M18_process_sync/L05_named_condvar.c
+++ appendix_extreme_c/M18_process_sync/L05_named_condvar.c
@@ -12,6 +12,32 @@
  * management with a OPP approach. So for this lesson, check the code divided in the files for
  * implementing the clases:
  * 
- *  - 
- * 
+ * - sh_mem: Which is the class (parent class) for shared memory object, so you can initialize 
+ *    and use them.
+ * - counter: Class related with the objective to implement (counting) following POSIX standard.
+ * - sh_mutex: Which is a child class from shared memory, with the option to create a shared mutex.
+ * - sh_cv: Similar to the previous cases, but it the child of the sh_mem and sh_mutex classes.
 */
+
+// Import for using the classes implemented with OOP in C
+#include "L05_counter.h"
+#include "L05_sh_mutex.h"
+#include "L05_sh_cv.h"
+
+int signal_rv = 0;
+struct sh_cv_t* cv = NULL;
+struct sh_mutex* mx = NULL;
+
+void singal_handler(int signal);
+
+int main(int argc, char const **argv)
+{
+    
+    return 0;
+}
+
+void singal_handler(int signal)
+{
+    fprintf(stdout, "\nINT signal received: %d ...\n", signal);
+    signal_rv = 1;
+}
--- appendix_extreme_c/M18_process_sync/L05_sh_cv.c
+++ appendix_extreme_c/M18_process_sync/L05_sh_cv.c
@@ -4,12 +4,18 @@
 #include "L05_sh_mutex.h"
 #include "L05_sh_mem.h"
 
+// Ttribute struc definition
 typedef struct
 {
     struct sh_mem_t* shm;
     pthread_cond_t* ptr;
 } sh_cv_t;
 
+/**
+ * Manually allocate a new region for a shared condition variable object
+ * 
+ * @return Adress to allocate the object.
+*/
 sh_cv_t* sh_cv_new()
 {
     sh_cv_t* shcv = (sh_cv_t*)malloc(sizeof(sh_cv_t));
@@ -17,12 +23,29 @@ sh_cv_t* sh_cv_new()
     return shcv;
 }
 
+/**
+ * Destroy and clear shared condition variable object.
+ * 
+ * @param shcv POinter to the shared condition variable object of interest.
+*/
 void sh_cv_delete(sh_cv_t* shcv)
 {
     sh_mem_delete(shcv->shm);
     free(shcv);
 }
 
+/**
+ * Constructor of a shared condition variable object.
+ * 
+ * @param shcv Adress to initialize the object.
+ * @param name Name of the shared resource.
+ * 
+ * @exception Launches an error and exits if the attributes for the cv couldn't be initialized.
+ * @exception Launches an error and exits if the attributes for the cv aren't set.
+ * @exception Launches an error and exits if the cv isn't initialized.
+ * @exception Launches an error and exits if the attributes cannot be destroyed.
+ * 
+*/
 void sh_cv_ctor(sh_cv_t* shcv, const char* name)
 {
     sh_mem_ctor(shcv->shm, name, sizeof(pthread_cond_t));
@@ -58,6 +81,13 @@ void sh_cv_ctor(sh_cv_t* shcv, const char* name)
     }
 }
 
+/**
+ * Destructor of a shared condition variable object.
+ * 
+ * @param shcv POinter to the shared cv object of interest.
+ * 
+ * @exceptionLaunches a arn if the condition variable couldn't be destroyed.
+*/
 void sh_cv_dtor(sh_cv_t* shcv)
 {
     if(sh_mem_isowner(shcv->shm))
@@ -71,6 +101,14 @@ void sh_cv_dtor(sh_cv_t* shcv)
     }   
 }
 
+/**
+ * Waiting operation for a condition variable that was successfuly set.
+ * 
+ * @param shcv Pointer to the shared condition variable object of interest.
+ * @param shx Pointer to the shared mutex object, which gives the control mechanism.
+ * 
+ * @exception LAunches and error and exits if the wait operation fails. 
+*/
 void sh_cv_wait(sh_cv_t* shcv, struct sh_mutex_t* shx)
 {
     int status = -1;
@@ -81,6 +119,14 @@ void sh_cv_wait(sh_cv_t* shcv, struct sh_mutex_t* shx)
     }
 }
 
+/**
+ * Waiting operation plus timer for a condition variable.
+ * 
+ * @param shcv Pointer to the shared condition variable object of interest.
+ * @param shx Pointer to the shared mutex object, which gives the control mechanism.
+ * @param time_check Time passed in nanoseconds.
+ * 
+*/
 void sh_cv_timedwait(sh_cv_t* shcv, struct sh_mutex_t* shx, long int time_check)
 {
     int status = -1;
@@ -107,7 +153,14 @@ void sh_cv_timedwait(sh_cv_t* shcv, struct sh_mutex_t* shx, long int time_check)
     }
 }
 
-void sh_cv_broadcast(struct sh_cv_t*)
+/**
+ * Broadcaster for shared condition variable object
+ * 
+ * @param shcv Pointer to the shared condition variable object of interest.
+ * 
+ * @exception Launches an error and exits if the broadcast isn't achieved with success.
+*/
+void sh_cv_broadcast(sh_cv_t* shcv)
 {
     int status = -1;
     if((status = pthread_cond_broadcast(shcv->ptr)))
