11d396c4d30af0e1d82cf4ecf26f03be8e1d84be
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2023-12-31 17:01:09 -0500

    Extrem C P19: POSIX msg queues and intro to socket programming

--- appendix_extreme_c/M19_single_host_ipc/L04_posix_queue_msg.c
+++ appendix_extreme_c/M19_single_host_ipc/L04_posix_queue_msg.c
@@ -14,25 +14,83 @@
  * - Message queues always open a file, which can be used for other processes.
  * - Message queues can be prioritized.
  * 
+ * Check the code below, and see how message queues works in a basic example.
+ * 
+ *      gcc L04_posix_queue_msg.c -lrt -o mq.out
+ *      ./mq.out
+ * 
  * NOTE: Message queues aren't available on macOS.
+ * 
+ * You may notice that without a consumer, the content can be acumulated and then block the process,
+ * and this is the main limitation of pipes and messages queues. One alternative can be the socktes.
 */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <mqueue.h>
+#include <mqueue.h> // Header needed for using message queues
 
-int main(int argc, char **argv)
+#define BUFFER_SIZE 32 // Size of buffer for reading
+
+int main(int argc, char** argv)
 {
     printf("\tExample of using POSIX message queues:\n");
 
     mqd_t msg_queue;
 
-    struct msq_queue_attr attr;
+    // Structu for defining attributes of messages
+    struct mq_attr attr;
     attr.mq_flags = 0;
     attr.mq_maxmsg = 10;
-    attr.mq_msgsize = 32;
+    attr.mq_msgsize = BUFFER_SIZE;
     attr.mq_curmsgs = 0;
+
+    // Fork process to use communication between parent and child
+    int child = fork();
+    if(child == -1)
+    {
+        fprintf(stderr, "ERROR: Couldn't fork process!\n");
+        exit(1);
+    }
+
+    // The child will be the sender
+    if(child == 0)
+    {
+        // Delay of one second, wait time for parent to be ready
+        sleep(1);
+
+        // Open message queue shared resource in write only mode
+        mqd_t msg_queue = mq_open("/mq0", O_WRONLY);
+
+        // Write msg, log and wait
+        char msg[] = "Hello World from Child!";
+        fprintf(stdout, "CHILD will wait 2 seconds...\n");
+        sleep(2);
+        fprintf(stdout, "CHILD is writting to father...\n");
+
+        // Send message and close channel for child
+        mq_send(msg_queue, msg, strlen(msg) + 1, 0);
+        mq_close(msg_queue);
+    }
+
+    // Paren will be the reciever
+    else
+    {
+        // Open shared memory in read only mode (and creates it if it doesn't exists)
+        mqd_t msg_queue = mq_open("/mq0", O_RDONLY | O_CREAT, 0644, &attr);
+        
+        // Create buffer and log ready
+        char buffer[BUFFER_SIZE];
+        fprintf(stdout, "FAHTER is reading from CHILD...\n");
+
+        // Read message and display it
+        int size_msg = mq_receive(msg_queue, buffer, BUFFER_SIZE, NULL);
+        fprintf(stdout, "Father received a message: %s\n", buffer);
+        
+        // Close and unlink shared memory region for message queue
+        mq_close(msg_queue);
+        mq_unlink("/mq0");
+    }
 
     printf("If you want to know more, explore the next links:\n");
     printf("- https://users.pja.edu.pl/~jms/qnx/help/watcom/clibref/mq_overview.html. \n");
---
+++ appendix_extreme_c/M19_single_host_ipc/L05_unix_dom_sockets.c
@@ -0,0 +1,13 @@
+// BASED ON THE "EXTREM C BOOK - 1 EDITION"
+// Code was tested with gcc
+
+#include <stdio.h>
+
+/**
+ * UNIX DOMAIN SOCKETS: They are sockets that can operate within the same machine, and they can be
+ * biderectional (so one can read/write over the same channel), and they are more flexible because
+ * they are 'session-aware' and 'message-aware'.
+ * 
+ * Sockets can be single or multi host and they use more or less the same API  and share concepts,
+ * and one is related with how computers networks works.
+*/
