6c1ee835f6133833a203d3fc8ccedf53b6ae8fd8
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-01-14 15:49:44 -0500

    Extrem C P20: Comments related with calculator service

--- appendix_extreme_c/M20_socket_programming/ch20-socket-programming/calcsvc/calc_service.c
+++ appendix_extreme_c/M20_socket_programming/ch20-socket-programming/calcsvc/calc_service.c
@@ -2,64 +2,159 @@
 
 #include "calc_service.h"
 
-struct calc_service_t {
+/**
+ * When you are ready and understand this part, you can go to the part that talks about unix domain
+ * sockets, the first file to check is: ""
+*/
+
+// Attribute structure for the service
+struct calc_service_t 
+{
   double mem;
 };
 
-struct calc_service_t* calc_service_new() {
+/**
+ * Manually allocate a new service object.
+ * 
+ * @return Adress of the allocated object by a pointer
+*/
+struct calc_service_t* calc_service_new() 
+{
   return (struct calc_service_t*)malloc(sizeof(struct calc_service_t));
 }
 
-void calc_service_delete(struct calc_service_t* svc) {
+/**
+ * Delete service object and free memory
+ * 
+ * @param svc Pointer to service object in use.
+*/
+void calc_service_delete(struct calc_service_t* svc) 
+{
   free(svc);
 }
 
-void calc_service_ctor(struct calc_service_t* svc) {
+/**
+ * Constructor of service object that only initialize memory value with zero
+ * 
+ * @param svc Pointer to service object in use
+*/
+void calc_service_ctor(struct calc_service_t* svc) 
+{
   svc->mem = 0.0;
 }
 
+/**
+ * Void constructor for service objects.
+ * 
+ * @param svc Pointer to service object in use
+*/
 void calc_service_dtor(struct calc_service_t* svc) {}
 
-void calc_service_reset_mem(struct calc_service_t* svc) {
+/**
+ * Reset memory (upate to zero)
+ * 
+ * @param svc Pointer to service object in use
+*/
+void calc_service_reset_mem(struct calc_service_t* svc) 
+{
   svc->mem = 0.0;
 }
 
-double calc_service_get_mem(struct calc_service_t* svc) {
+/**
+ * Getter to value stored in memory
+ * 
+ * @param svc Pointer to service object in use
+ * 
+ * @return Double value currently stored in object.
+*/
+double calc_service_get_mem(struct calc_service_t* svc) 
+{
   return svc->mem;
 }
 
+/**
+ * Addition function that has two option: Add two numbers or additionally add value
+ * stored in memory.
+ * 
+ * @param svc Pointer to service object in use
+ * @param a Operand 1
+ * @param b Operand 2
+ * @param mem Boolean that indicates if stored value is added
+ * 
+ * @return Sum according the operands passed
+*/
 double calc_service_add(struct calc_service_t* svc, double a, double b,
-    bool_t mem) {
+    bool_t mem) 
+{
   double result = a + b;
-  if (!mem) {
+  if (!mem) 
+  {
     return result;
   }
   svc->mem = result + svc->mem;
   return svc->mem;
 }
 
+/**
+ * Substraction function that has two options: Substraction between two operands or
+ * additionally consider memory.
+ * 
+ * @param svc Pointer to service object in use
+ * @param a Operand 1
+ * @param b Operand 2
+ * @param mem Boolean that indicates if stored value is considered
+ * 
+ * @return The difference of the operands specified
+*/
 double calc_service_sub(struct calc_service_t* svc, double a, double b,
-    bool_t mem) {
+    bool_t mem) 
+{
   double result = a - b;
-  if (!mem) {
+  if (!mem) 
+  {
     return result;
   }
   svc->mem = result - svc->mem;
   return svc->mem;
 }
 
+/**
+ * Multiplication function with two options: Multiplication between two factors or
+ * additionally consider the value stored.
+ * 
+ * @param svc Pointer to service object in use
+ * @param a Operand 1
+ * @param b Operand 2
+ * @param mem Boolean that indicates if stored value is considered
+ * 
+ * @return Product of the operands specified
+*/
 double calc_service_mul(struct calc_service_t* svc, double a, double b,
-    bool_t mem) {
+    bool_t mem) 
+{
   double result = a * b;
-  if (!mem) {
+  if (!mem) 
+  {
     return result;
   }
   svc->mem = result * svc->mem;
   return svc->mem;
 }
 
-int calc_service_div(struct calc_service_t* svc, double a, double b, double* result) {
-  if (b == 0.0) {
+/**
+ * Division function that prevents the case of zero division
+ * 
+ * @param svc Pointer to service object in use
+ * @param a Dividend
+ * @param b Divisor
+ * @param result Pointer that will be used to update result
+ * 
+ * @return Status ok or status zero division error, respective the case.
+*/
+int calc_service_div(struct calc_service_t* svc, double a, double b, double* result) 
+{
+  if (b == 0.0) 
+  {
     return CALC_SVC_ERROR_DIV_BY_ZERO;
   }
   *result = a / b;
--- appendix_extreme_c/M20_socket_programming/ch20-socket-programming/calcsvc/calc_service.h
+++ appendix_extreme_c/M20_socket_programming/ch20-socket-programming/calcsvc/calc_service.h
@@ -1,19 +1,36 @@
 #ifndef CALC_SERVICE_H
 #define CALC_SERVICE_H
 
+/**
+ * This service takes care of the real calculation process, and it doesn't use IPC, as it is 
+ * defined in the socket programming part. So it means, that this service can be used in a main 
+ * function.
+ * 
+ * Keep in mind, that some calculations won't need a specific context, so you can just have a single
+ * service object to handle it (stateless service object). But there is also the possibility that a
+ * object would request a previous request then, you will need a unique service object for each
+ * client.
+ *
+ * Now, go to the 'calc_service.c' to understand better the functions implemented.
+*/
 #include <types.h>
 
+// Calculation status
 static const int CALC_SVC_OK = 0;
 static const int CALC_SVC_ERROR_DIV_BY_ZERO = -1;
 
+// Forward declaration
 struct calc_service_t;
 
+// Memory management function
 struct calc_service_t* calc_service_new();
 void calc_service_delete(struct calc_service_t*);
 
+// Constructor and destructor
 void calc_service_ctor(struct calc_service_t*);
 void calc_service_dtor(struct calc_service_t*);
 
+// Methods of the service class
 void calc_service_reset_mem(struct calc_service_t*);
 double calc_service_get_mem(struct calc_service_t*);
 double calc_service_add(struct calc_service_t*, double, double b,
