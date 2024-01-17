a159a08d4c454fc31b98aa4f6f3a2732771cd4b1
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-01-17 10:05:59 -0500

    Extrem C P20: Comments realted with sockets in the client stream functionality

--- appendix_extreme_c/M20_socket_programming/ch20-socket-programming/client/clicore/stream_client_core.c
+++ appendix_extreme_c/M20_socket_programming/ch20-socket-programming/client/clicore/stream_client_core.c
@@ -9,16 +9,29 @@
 
 #include "common_client_core.h"
 
-void* stream_response_reader(void* obj) {
+/**
+ * Reader of the buffer to check if it is valid to be serialized
+ * 
+ * @param obj Generic pointer to context obj
+ * 
+ * @return NULL if completed
+ * 
+ * @exception Launches an error and exits if the reading was not possible
+*/
+void* stream_response_reader(void* obj)
+{
   struct context_t* context = (struct context_t*)obj;
   char buf[64];
-  while (1) {
+  while (1) 
+  {
     int ret = read(context->sd, buf, sizeof(buf));
-    if (ret < 0) {
+    if (ret < 0) 
+    {
       fprintf(stderr, "Read error! %s\n", strerror(errno));
       break;
     }
-    if (ret == 0) {
+    if (ret == 0) 
+    {
       break;
     }
     struct buffer_t b; b.data = buf, b.len = ret;
@@ -27,47 +40,71 @@ void* stream_response_reader(void* obj) {
   return NULL;
 }
 
-void stream_client_loop(int conn_sd) {
+/**
+ * Stream client that reads messages, serializes and write it to send the request.
+ * 
+ * @param conn_sd Socket file descriptor for connections
+*/
+void stream_client_loop(int conn_sd) 
+{
+  // Generate context (for socket file descriptor)
   struct context_t context;
-
   context.sd = conn_sd;
+
+  // Generate new serialization object
   context.ser = calc_proto_ser_new();
   calc_proto_ser_ctor(context.ser, &context, 128);
   calc_proto_ser_set_resp_callback(context.ser, on_response);
   calc_proto_ser_set_error_callback(context.ser, on_error);
 
+  // Instance thread for reading the content on the file descriptor
   pthread_t reader_thread;
   pthread_create(&reader_thread, NULL,
           stream_response_reader, &context);
 
   char buf[128];
   printf("? (type quit to exit) ");
-  while (1) {
+  while (1) 
+  {
+    // Read input provided
     scanf("%s", buf);
     int brk = 0, cnt = 0;
     struct calc_proto_req_t req;
+
+    // Analize buffer and check it is valid
     parse_client_input(buf, &req, &brk, &cnt);
-    if (brk) {
+    if (brk)
+    {
       break;
     }
-    if (cnt) {
+    if (cnt) 
+    {
       continue;
     }
+
+    // Serialize request and write into the socket file descriptor
     struct buffer_t ser_req =
         calc_proto_ser_client_serialize(context.ser, &req);
     int ret = write(context.sd, ser_req.data, ser_req.len);
-    if (ret == -1) {
+    
+    // Check if the writting process was succesfully achieved
+    if (ret == -1) 
+    {
       fprintf(stderr, "Error while writing! %s\n",
               strerror(errno));
       break;
     }
-    if (ret < ser_req.len) {
+    if (ret < ser_req.len) 
+    {
       fprintf(stderr, "Wrote less than anticipated!\n");
       break;
     }
     printf("The req(%d) is sent.\n", req.id);
   }
+  // Close the connection socket descriptor
   shutdown(conn_sd, SHUT_RD);
+
+  // Destroy, close and free objects used
   calc_proto_ser_dtor(context.ser);
   calc_proto_ser_delete(context.ser);
   pthread_join(reader_thread, NULL);
--- appendix_extreme_c/M20_socket_programming/ch20-socket-programming/client/unix/stream/main.c
+++ appendix_extreme_c/M20_socket_programming/ch20-socket-programming/client/unix/stream/main.c
@@ -9,13 +9,26 @@
 
 #include <stream_client_core.h>
 
-int main(int argc, char** argv) {
+/**
+ * Once the server stream was implemented, we need to follow the connector sequence (UDS), but 
+ * for the connection you will need to specify a UDS endpoint.
+ * 
+ * The most notorious change from the server is the change of 'bind' to 'connect'.
+ * 
+ * WHEN READY: GO to the file: 'client/clicore/stream_client_core.c'
+*/
+
+int main(int argc, char** argv)
+{
+  // Socket file descriptor, must be the same of the server
   char sock_file[] = "/tmp/calc_svc.sock";
 
-  // ----------- 1. Create socket object ------------------
-
+  // ----------- 1. Create socket object ----------------------------------------------------------
+ 
+  // The socket type must be congruent with the specified in the server
   int conn_sd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (conn_sd == -1) {
+  if (conn_sd == -1) 
+  {
     fprintf(stderr, "Could not create socket: %s\n",
             strerror(errno));
     exit(1);
@@ -29,9 +42,11 @@ int main(int argc, char** argv) {
   addr.sun_family = AF_UNIX;
   strncpy(addr.sun_path, sock_file, sizeof(addr.sun_path) - 1);
 
+  // Make connection (instaed of bind) and check if it is valid
   int result = connect(conn_sd,
           (struct sockaddr*)&addr, sizeof(addr));
-  if (result == -1) {
+  if (result == -1) 
+  {
     close(conn_sd);
     fprintf(stderr, "Could no connect: %s\n", strerror(errno));
     exit(1);
