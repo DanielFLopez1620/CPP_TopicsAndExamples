0f65a1a3768ae18fc0e7ad23840b814be184f2dd
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-01-25 10:04:50 -0500

    Extrem C P21: Intro to language integration, import of libraries but there pending LICENSES

---
+++ appendix_extreme_c/M21_language_integration/L01_integration_is_possible.c
@@ -0,0 +1,30 @@
+// BASED ON THE "EXTREM C BOOK - 1 EDITION"
+// Code was tested with gcc
+
+#include <stdio.h>
+
+/**
+ * Many higher-level languages were writtten in C, and they have been developed with tools and
+ * compilers written in C. Remeber that a programming language objective is to generate an program
+ * that works, and it is also important to be able to use the functionalities exposed by the 
+ * Operating System. You have already read about the C Standard, but there are others, for example,
+ * Jave SE (Java Standard Edition).
+ * 
+ * Let's talk briefly about Java, if you didn't know, Java is compiled by using an intermediate 
+ * language called 'bytecode', and for this you will ned the JRE (Java Runtime Environment) 
+ * installed, which provides a virtual machine that is able to simulate the functions exposed by
+ * the C standard library.
+ * 
+ * We are gonna make some implementatioons of C that will be able to work in other languages.
+ * Similar to the situation presented in the last module, we are gonna use the original code, and then we
+ * are gonna go through a exploration by making comments.
+ * 
+ * WHEn READY: Go to the project folder and locate the file 'cstack.h'
+*/
+int main(int argc, char const *argv[])
+{
+    printf("C was the Key for implementing general-purpose languages\n");
+    printf("Our objective is going to create a lirbary and use it in other languages...\n");
+    printf("Stay tunned for more!\n");
+    return 0;
+}
---
+++ appendix_extreme_c/M21_language_integration/ch21-integration-with-other-languages/cstack.h
@@ -0,0 +1,56 @@
+/* 
+ * File name: cstack.h
+ * Description: Declarations of the stack library
+ * 
+ * NOTE: It is called cstack to avoid confussion with the stack itsefl.
+*/
+
+#ifndef _CSTACK_H_
+#define _CSTACK_H_
+
+#include <unistd.h>
+
+#ifdef __cplusplus
+extern "C" 
+{
+  #endif
+
+  #define TRUE 1
+  #define FALSE 0
+
+  typedef int bool_t;
+
+  // Struct definition to handle data with this length
+  typedef struct 
+  {
+    char* data;
+    size_t len;
+  } value_t;
+
+  typedef struct cstack_type cstack_t;
+
+  typedef void (*deleter_t)(value_t* value);
+
+  value_t make_value(char* data, size_t len);
+  value_t copy_value(char* data, size_t len);
+  void free_value(value_t* value);
+
+  // Memory management for object
+  cstack_t* cstack_new();
+  void cstack_delete(cstack_t*);
+
+  // Behavior functions (constructor and destructor)
+  void cstack_ctor(cstack_t*, size_t);
+  void cstack_dtor(cstack_t*, deleter_t);
+
+  // Stack class methods
+  size_t cstack_size(const cstack_t*);
+  bool_t cstack_push(cstack_t*, value_t value);
+  bool_t cstack_pop(cstack_t*, value_t* value);
+  void cstack_clear(cstack_t*, deleter_t);
+
+  #ifdef __cplusplus
+}
+#endif
+
+#endif
