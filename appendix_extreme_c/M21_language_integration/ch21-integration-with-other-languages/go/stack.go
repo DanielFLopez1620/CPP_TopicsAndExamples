c9ecd938a1b7163499e55e81ccb2119f7a0357ab
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-02-02 09:29:36 -0500

    Extrem C P21: About Go(lang) integration and start moving to next module

--- appendix_extreme_c/M21_language_integration/ch21-integration-with-other-languages/go/stack.go
+++ appendix_extreme_c/M21_language_integration/ch21-integration-with-other-languages/go/stack.go
@@ -1,6 +1,21 @@
 // File name: stack.go
 // Description: Go wrapper around the cstack library
 
+
+/*
+Finally, we will explore Golang, it provides an easy integration with native shared libraries.
+It is called itself a system programming language as the future generation C or C++.
+
+But, let's move on to what interest us, and it is a package called 'cgo' to call C code
+and load the shared object files. So, go and check below, when you are done, you 
+can execute: 
+
+    cd go
+    go build -o stack.out stack.go
+    LD_LIBRARY_PATH=$PWD/.. ./stack.out
+
+WHEN READY: Move to the nex module
+*/
 package main
 
 /*
@@ -8,15 +23,17 @@ package main
 #cgo LDFLAGS: -L.. -lcstack
 #include <cstack.h>
 */
-import "C"
+import "C" // Import C
 import (
   "fmt"
 )
 
+// Create a handler struct to use the C custom stack
 type Stack struct {
   handler *C.cstack_t
 }
 
+// New Stack function that wraps the C implementation
 func NewStack() *Stack {
   s := new(Stack)
   s.handler = C.cstack_new()
@@ -24,21 +41,25 @@ func NewStack() *Stack {
   return s
 }
 
+// Destructor wrapped of the C implementation
 func (s *Stack) Destroy() {
   C.cstack_dtor(s.handler, C.deleter_t(C.free_value))
   C.cstack_delete(s.handler)
 }
 
+// Wrapped getter of the size of the custom stack
 func (s *Stack) Size() int {
   return int(C.cstack_size(s.handler))
 }
 
+// Wrapped push function of the C custom Stack
 func (s *Stack) Push(item string) bool {
   value := C.make_value(C.CString(item), C.ulong(len(item) + 1))
   pushed := C.cstack_push(s.handler, value)
   return pushed == 1
 }
 
+// Wrapped pop function of the C custom Stack 
 func (s *Stack) Pop() (bool, string) {
   value := C.make_value(nil, 0)
   popped := C.cstack_pop(s.handler, &value)
@@ -47,19 +68,29 @@ func (s *Stack) Pop() (bool, string) {
   return popped == 1, str
 }
 
+// Wrapped clear all function of the C custom Stack
 func (s *Stack) Clear() {
   C.cstack_clear(s.handler, C.deleter_t(C.free_value))
 }
 
+// Main implementation of Golang
 func main() {
+
+  // Create new stack
   var stack = NewStack()
+
+  // Push elements
   stack.Push("Hello")
   stack.Push("World")
   stack.Push("!")
   fmt.Println("Stack size:", stack.Size())
+
+  // Pop elements
   for stack.Size() > 0 {
     _, str := stack.Pop()
     fmt.Println("Popped >", str)
   }
+
+  // Destroy
   stack.Destroy()
 }
---
+++ appendix_extreme_c/M22_unit_testing/L01_about_unit_test.c
@@ -0,0 +1,20 @@
+// BASED ON THE BOOK EXTREM C - 1° Edition
+// Code tested with gcc
+
+/**
+ * Testing is always an important part of developing, no matter the programming language and it is 
+ * a must for a real project.
+ * 
+ * The first question to bring to the table is.... What are we testing? What is the test about?
+ * So, we can consider a 'functional' (functionality in general, related with requirementes, modules,
+ * componentes, systems that expect a certain output) or 'non-functional'(quality level with memory, 
+ * complexity, time to completation and security) approaches.
+ * 
+ * But for this we will need to focus on certain levels, that are:
+ * 
+ * - UNIT TESTING:
+ * - INTEGRATION TESTING:
+ * - SYSTEM TESTING:
+ * - ACCEPTANCE TESTING:
+ * - REGRESSION TESTING:
+*/
