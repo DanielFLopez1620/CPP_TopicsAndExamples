3fcf03a909dd29f44340acfb6bb11cc8abf5a27b
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-02-08 10:31:11 -0500

    Extrem C P22: Adding more info of CMoka

--- appendix_extreme_c/M22_unit_testing/L03_cmoka_test.c
+++ appendix_extreme_c/M22_unit_testing/L03_cmoka_test.c
@@ -11,6 +11,22 @@
  * It is 'de facto' in testing C programs, it offers test fixtures to initialize and clear testing
  * environments before and after each test case. Also functions function mocking (well... this is
  * obvious) and it is very useful, here in this code we will implement a test with this library.
+ * 
+ * One of the main 'to-do' for tests with CMocka is to implement tast cases that return void and
+ * receive void** arguments, as this will allow to use 'states' and receive them to get the
+ * feedback. Also, you will need to implement to functions 'setup' and 'tear_down', which are test
+ * fixtures called before and after the test case to do what indicates her name for each test case.
+ * 
+ * NOTE: If you do not have CMocka, you can install it in Ubuntu with:
+ * 
+ *      sudo apt-get install libcmocka-dev
+ * 
+ * For the execution of this code, you can run:
+ * 
+ *      gcc -g -c L02_fibonacci.c -o impl.o
+ *      gcc -g -c L03_cmoka_test.c -o cmoka_test.o
+ *      gcc impl.o cmoka_test.o -lcmocka -o exe_moka_test.out
+ *      ./exe_moka_test.out
 */
 
 //  Libraries required for Cmocka:
@@ -62,7 +78,24 @@ void next_number__numbers_should_rotate()
     assert_not_equal(num1, num2);
 }
 
+int setup(void** state)
+{
+    return 0;
+}
+
+int tear_down(void** state)
+{
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
-    
+    const struct CMUnitTest tests[] = {
+        cmoka_unit_test(fibonacci__fib_of_one_is_one),
+        cmoka_unit_test(fibonacci__fib_of_zero_is_zero),
+        cmoka_unit_test(fibonnaci__fib_of_four_is_three),
+        cmoka_unit_test(next_number__numbers_should_rotate)
+    }
+
+    return cmoka_run_group_tests(tests, setup, tear_down);
 }
