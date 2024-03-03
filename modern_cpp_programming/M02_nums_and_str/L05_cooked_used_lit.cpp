d00dec36a00a783a16be48682aef4efc1892a4b1
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-03-03 09:50:55 -0500

    Modern C++ P02: Using cooked literals

--- modern_cpp_programming/M02_nums_and_str/L05_cooked_used_lit.cpp
+++ modern_cpp_programming/M02_nums_and_str/L05_cooked_used_lit.cpp
@@ -27,6 +27,12 @@
  *      T operator "" _suffix(char16_t const *, std::size_t);
  *      T operator "" _suffix(char32_t const *, std::size_t);
  * 
+ * Keep in mind that for integral literals the operator taks a unsigned long
+ * long and the raw literal operator taks a const char*, for floating-point
+ * literals you use long double and const char*, for character literals you
+ * should use the appropriate operator depdending on the character type
+ * (char, wchar_t, char16_t, char32_t) and for strings consider the type of
+ * string (with a pointer) and the size.
 */
 
 #include <array>
@@ -49,16 +55,66 @@ namespace packing
     enum class pack
     {
         kilogram,
-        squeared_meters,
+        squared_meters,
         num_elements,
     };
 
     template <pack P>
-    //class 
+    class quantity_pack
+    {
+        const double amount;
+    public:
+        constexpr explicit quantity_pack(double const a) : unity(a) {}
+        explicit operator double() const { return amount; } 
+    };
 
+    template <pack P>
+    constexpr quantity_pack<P> operator+(quantity_pack<P> const &p1, 
+                                         quantity_pack<P> const &p2)
+    {
+        return quantity_pack<P>(
+            static_cast<double>(p1) + static_cast<double>(p2));
+    }
 
+    template <pack P>
+    constexpr quantity_pack<P> operator-(quantity_pack<P> const &p1,
+        quantity_pack<P> const &p2)
+    {
+        return quantity_pack<P>(
+            static_cast<double>(p1) - static_cast<double>(p2));
+    }
+
+    namespace quantity_pack_literals
+    {
+        constexpr quantity_pack<pack::kilogram> operator "" _kg (
+            long double const amount)
+        {
+            return quantity_pack<pack::kilogram> 
+                { static_cast<double>(amount) };
+        }
+
+        constexpr quantity_pack<pack::kilogram> operator "" _kg (
+            unsigned long long const amount)
+        {
+            return quantity_pack<pack::kilogram> 
+                { static_cast<double>(amount) };
+        }
+
+        constexpr quantity_pack<pack::squared_meters> operator "" _m2 (
+            long double const amount)
+        {
+            return quantity_pack<pack::squared_meters> 
+                { static_cast<double>(amount) };
+        }
+
+        constexpr quantity_pack<pack::num_elements> operator "" _pcs (
+            unsigned long long const amount)
+        {
+            return quantity_pack<pack::num_elements> 
+                { static_cast<double>(amount) };
+        }
+    }
 }
-
 
 
 int main(int argc, char** argv)
@@ -71,5 +127,14 @@ int main(int argc, char** argv)
     std::cout << "Example of literal 2 MB to bytes: " << mb_size << std::endl;
 
     // Info #2:
+    using namespace packing;
+    using namespace quantity_pack_literals;
+
+    std::cout << "Let's obtain the info of the package #1: " << std::endl;
+    auto p1_kg = { 1_kg };
+    auto p1_m2 = { 0.25_m2 };
+    auto p1_pcs = { 20_pcs };
+    std::cout << "Weight: " << p1_kg << "kg" << std::endl << "Area: " << p1_m2 
+              << " m^2" << std::endl << "Pieces: " << p1_pcs << std::endl;
     return 0;
 }
