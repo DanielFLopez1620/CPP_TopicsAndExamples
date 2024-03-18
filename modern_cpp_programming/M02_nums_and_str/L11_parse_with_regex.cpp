db33bd4f2e585b67ac044012b73590c99e2059f8
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-03-18 08:35:59 -0500

    Modern C++ P02: Parse with regex not working, still pending correction

--- modern_cpp_programming/M02_nums_and_str/L11_parse_with_regex.cpp
+++ modern_cpp_programming/M02_nums_and_str/L11_parse_with_regex.cpp
@@ -25,30 +25,33 @@
 #include <regex>
 #include <string>
 
-using namespace std::string_literals;
+
 
 int main(int argc, char** argv)
 {
+    using namespace std::string_literals;
+
     // Example text that will be used for identify with regex.
     auto info
     {
         R"(
-            timeout=100
-            server = 127.0.0.0
-            port = 11111
+timeout=100
+server = 127.0.0.0
+port=11111
         )"s
     };
 
     // Info #1: Do not forget on how to create a pattern for a regex:
-    auto pattern {R"(^(?!#)(\w+)\s*=\s*([\w\d]+[\w\d._\-:]*)$)"s};
-    auto regex = std::regex{pattern};
+    auto pattern {R"(^(?!#)(\w+)\s*=\s*([\w\d]+[\w\d._,\-:]*)$)"s};
+    auto rgx = std::regex{R"(^(\w+)\s*=\s*([\w\d.]+)$)"};
 
     // Info #2: For searching occurrences (well, the first one), you can use:
+    
     auto match = std::smatch{};
-    if(std::regex_search(info, match, regex))
+    if (std::regex_search(info, match, rgx)) 
     {
-        std::cout << "Searching for first ocurrence: " << std::endl 
-                  << match[1] << " = " << match[2] << std::endl << std::endl;
+        std::cout << "Searching for first occurrence: " << std::endl
+                << match[1] << " = " << match[2] << std::endl << std::endl;
     }
 
     // Info #3: You can also use iterators with the matches for the regex
@@ -56,7 +59,7 @@ int main(int argc, char** argv)
     std::cout << "Checking iterations: " << std::endl;
     auto end = std::sregex_iterator{};
     for (auto it = std::sregex_iterator{
-        std::begin(info), std::end(info), regex}; it != end; ++it)
+        std::begin(info), std::end(info), rgx}; it != end; ++it)
     {
         std::cout << '\'' << (*it)[1] << "'='"
                   << (*it)[2] << '\'' << std::endl;
@@ -67,7 +70,7 @@ int main(int argc, char** argv)
     std::cout << "Checking iterations with tokens: " << std::endl;
     auto end_tok = std::sregex_token_iterator{};
     for (auto it = std::sregex_token_iterator{
-        std::begin(info), std::end(info), regex }; it != end_tok; ++it)
+        std::begin(info), std::end(info), rgx }; it != end_tok; ++it)
     {
         std::cout << "\t" << *it << std::endl;
     }
