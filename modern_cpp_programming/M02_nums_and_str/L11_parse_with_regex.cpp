87f49bacfac2cce5117aabaddd3fe2d92a155f2b
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-03-19 10:03:17 -0500

    Modern C++: Exploring matches with regex

--- modern_cpp_programming/M02_nums_and_str/L11_parse_with_regex.cpp
+++ modern_cpp_programming/M02_nums_and_str/L11_parse_with_regex.cpp
@@ -32,48 +32,56 @@ int main(int argc, char** argv)
     using namespace std::string_literals;
 
     // Example text that will be used for identify with regex.
-    auto info
-    {
-        R"(
-timeout=100
-server = 127.0.0.0
-port=11111
-        )"s
-    };
+    std::string lines[] = {"Roses are #ff0000",
+                    "violets are #0000ff",
+                    "all of my base are belong to you"};
 
     // Info #1: Do not forget on how to create a pattern for a regex:
-    auto pattern {R"(^(?!#)(\w+)\s*=\s*([\w\d]+[\w\d._,\-:]*)$)"s};
-    auto rgx = std::regex{R"(^(\w+)\s*=\s*([\w\d.]+)$)"};
+    auto pattern {R"( #([a-f0-9]{2})\n ([a-f0-9]{2}) "([a-f0-9]{2})\n )"s};
 
-    // Info #2: For searching occurrences (well, the first one), you can use:
+    std::regex rgx = std::regex{ "#([a-f0-9]{2})"
+                                  "([a-f0-9]{2})"
+                                  "([a-f0-9]{2})" };
+
+    // Info #2: Usage of regex_search for matching patterns, opr validating if
+    // pattern is present.
+    std::cout << "Searching for color pattern: " << std::endl
+              << pattern << std::endl << std::endl
+              << "Lines containts the pattern? " << std::endl;
+    for (const auto& line : lines)
+    {
+        std::cout << line << ":= " << std::boolalpha 
+                  << std::regex_search(line, rgx) << std::endl;
+    }
+    std::cout << std::endl << "Now displaying matches:\n" << std::endl;
+
+    std::smatch color_match;
+    for (const auto& line : lines)
+    {
+        if (std::regex_search(line, color_match, rgx))
+        {
+            std::cout << "matches for '" << line << "'\n";
+            std::cout << "Prefix: '" << color_match.prefix() << "'\n";
+            for (std::size_t i = 0; i < color_match.size(); ++i) 
+                std::cout << i << ": " << color_match[i] << '\n';
+            std::cout << "Suffix: '" << color_match.suffix() << "\'\n\n";
+        }
+    }
+
+    // Info #2: Using iterator with regex for searching occasions.
+    std::string lines_str = {"Roses are #ff0000 \n violets are #0000ff \n all of my base are belong to you"};
+
+    auto match_begin = std::sregex_iterator(lines_str.begin(), lines_str.end(), rgx);
+    auto match_end = std::sregex_iterator();
+
+    for (std::sregex_iterator i = match_begin; i != match_end; ++i)
+    {
+        std::smatch patter_match = *i;
+        std::string str_match = patter_match.str();
+        std::cout << str_match << std::endl;
+    }
+
     
-    auto match = std::smatch{};
-    if (std::regex_search(info, match, rgx)) 
-    {
-        std::cout << "Searching for first occurrence: " << std::endl
-                << match[1] << " = " << match[2] << std::endl << std::endl;
-    }
-
-    // Info #3: You can also use iterators with the matches for the regex
-    // and check for the occurences.
-    std::cout << "Checking iterations: " << std::endl;
-    auto end = std::sregex_iterator{};
-    for (auto it = std::sregex_iterator{
-        std::begin(info), std::end(info), rgx}; it != end; ++it)
-    {
-        std::cout << '\'' << (*it)[1] << "'='"
-                  << (*it)[2] << '\'' << std::endl;
-    }
-
-    // Info #4: You can also iterate in the subexpressiones, by considering
-    // tokens:
-    std::cout << "Checking iterations with tokens: " << std::endl;
-    auto end_tok = std::sregex_token_iterator{};
-    for (auto it = std::sregex_token_iterator{
-        std::begin(info), std::end(info), rgx }; it != end_tok; ++it)
-    {
-        std::cout << "\t" << *it << std::endl;
-    }
 
 
     return 0;
