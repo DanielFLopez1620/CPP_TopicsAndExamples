64de1ebba222a50b9e96dabe71ba6849f138c38a
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-03-27 11:12:37 -0500

    Modern C++ M03: Talking about defaulted and deleted functions with OOP

--- modern_cpp_programming/M03_exploring_functions/L01_def_del_func.cpp
+++ modern_cpp_programming/M03_exploring_functions/L01_def_del_func.cpp
@@ -1,4 +1,6 @@
 // BASED ON THE "MODERN C++  PROGRAMMING COOKBOOK - 2 EDITION"
+// BASED TOO ON GEEKSFORGEEKS Website:
+// https://www.geeksforgeeks.org/explicitly-defaulted-deleted-functions-c-11/
 // Code was tested with g++ in C++11
 
 #include <iostream>
@@ -23,18 +25,69 @@
 class MySetOfFuncs
 {
 public:
+    // Info #1: This is a user-based constructor, the ones you made by 
+    // yourself in your code.
     MySetOfFuncs (std::string name)
     {
         std::cout << "Method made by: " << name << std::endl;
     }
     
+    // Info #2: When you have a user-defined constructor, the default 
+    // constructor isn't considered. So we add a constructor with an option
+    // '=default' where we specify it. Keep in mind it doesn't work with non-
+    // special members or special members with arguments.
+    // Having a default constructor allow you to have a trivial type and it can
+    // be used with dcopy constructors and destructors.
+    // The usage is shown below: 
     MySetOfFuncs() = default;
+
+    // Info #3: Delete back then was only used to deallocate memory, but after
+    // C++11 it also allows to disable the usage of a member function by
+    // adding '=delete'. This allows a cleaner way to prevent special members
+    // we do not want and prevents unintened type promotions.
+    // Here we show the usage to prevent a copy constructor and prevent doubles.
+    MySetOfFuncs(const MySetOfFuncs &) = delete;
+    MySetOfFuncs(double) = delete;
+
+    // Info #4: Another definition to prevent copies (or better called a non-
+    // copyable type), are:
+    MySetOfFuncs& operator=(MySetOfFuncs const&) = delete;
 };
 
+// Info #5: You can create a non-copyable object that is still movable by 
+// declaring the 'copy-operations' as deleted, then implementing the 'move
+// operations'. 
+struct example_type{};
+class NonCopyButMovable
+{
+    example_type* ex;
+public: 
+    // User defined constructor and destructor
+    NonCopyButMovable(example_type* ex_p = nullptr) : ex(ex_p) {}
+    ~NonCopyButMovable() {delete ex; }
+
+    // Making sure of non-copyable
+    NonCopyButMovable(NonCopyButMovable const&) = delete;
+    NonCopyButMovable& operator=(NonCopyButMovable const&) = delete;
+
+    // Making sure it is movable
+    NonCopyButMovable(NonCopyButMovable&& other):ex(std::move(other.ex))
+    {   other.ex = nullptr;   }
+};
+
+// In the main we will test our implementations:
 int main(int argc, char** argv)
 {
+    std::cout << "Check the code to understand the constructor that call"
+              << " the results shown below, so you can learn about defaulted"
+              << " and deleted functions" << std::endl;
+    
     // Info #: Calling the constructors implementations.
-    MySetOfFuncs a;           // Default
-    MySetOfFuncs name("Dan")  // Parameterized constructor
+    MySetOfFuncs a;             // Default
+    MySetOfFuncs name1("Dan");  // Parameterized constructor
+    MySetOfFuncs name2("DD");   // Parameterized constructor
+    // name1 = name2            // Error, copy isn't possible
+    // MySetOfFuncs dou1(1620)  // Error, double type param is disabled
+
     return 0;
 }
