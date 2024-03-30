470620a648eb955c8bef05d83983f0844d576afa
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-03-30 14:34:12 -0500

    Modern C++ M03: Using lambda functions with types, generics and others

--- modern_cpp_programming/M03_exploring_functions/L02_lambda_func.cpp
+++ modern_cpp_programming/M03_exploring_functions/L02_lambda_func.cpp
@@ -28,12 +28,40 @@ int main()
     auto nums = std::vector<int>{0, -1, -2, 3, 4, -5, 6, -7};
     auto negatives = std::count_if(std::begin(nums), std::end(nums), 
         [](int const num) { return num < 0; });
-    std::cout << "From numers array: " << nums << std::endl
-              << "Negative nums are: " << negatives << std::endl << std::endl;
+    std::string nums_str {""};
+    for(const auto& num : nums)
+    {
+        nums_str += std::to_string(num) + " ,";
+    }
+    std::cout << "From numers array: " << nums_str << std::endl
+              << "How many negatives?: " << negatives << std::endl << std::endl;
     
     // Info #2: Second type of lambda function, a expression for checking
     // approved notas (named lambda)
     auto notes = std::vector<float>{4.0, 1.0, 2.0, 3.5, 5.0, 4.2};
-    auto is_approved = [](float const num){return num >= 3.5};
+    auto is_approved = [](float const num){ return num >= 3.5; };
+    auto aproveds = std::count_if(std::begin(notes), std::end(notes), 
+        is_approved);
+    std::string notes_str {""};
+    for(const auto& note : notes)
+    {
+        notes_str += std::to_string(note) + " ,";
+    }
+    std::cout << "From notes array: " << notes_str << std::endl
+              << "How many approved?: " << aproveds << std::endl << std::endl;
+
+    // Info #3: You can also create your own generic lambdas (after C++14) by
+    // using auto in the arguments specification.
+    auto more_notes = std::vector<float>{5 , 4.8, 1, 1.0, 2, 3.5, 4.2, 4};
+    auto exceptionals = std::count_if(std::begin(more_notes), 
+        std::end(more_notes), [](auto const n){ return n > 4; });
+    std::string m_notes_str {""};
+    for(const auto& note : more_notes)
+    {
+        m_notes_str += std::to_string(note) + " ,";
+    }
+    std::cout << "From notes array: " << m_notes_str << std::endl
+            << "How many pretty good? " << aproveds << std::endl << std::endl;
+
     return 0;
 }
