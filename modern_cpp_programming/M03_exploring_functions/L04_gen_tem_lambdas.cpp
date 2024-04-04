1f64c0c8883583dc90f3aaadd656bac04153eaf7
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-04-04 10:03:19 -0500

    Modern C++ M03: More on generic lambdas with structs and templates

--- modern_cpp_programming/M03_exploring_functions/L04_gen_tem_lambdas.cpp
+++ modern_cpp_programming/M03_exploring_functions/L04_gen_tem_lambdas.cpp
@@ -20,6 +20,11 @@
  *   Computes the sum between init and the the elements provided in the range
  *    of first and last.
  * 
+ * - std::decay_t<decltype(x);
+ *   Used to deduce the type of the parameter x, decltype detects the type at
+ *   compile time and decay_t, remove any qualifier (const, volatile...) to 
+ *   leave the underlying type.
+ * 
  * Now, for C++20, there is the possibility of using template lambdas. And they
  * can be used to restrict the use of a generic lambda to only some types, make
  * sure that two or more arguments have the same type, retrieve the type of a
@@ -40,6 +45,18 @@ auto vector_lambda = []<typename T>(std::vector<T> const& vec)
     std::cout << "Size of vector is: " << std::size(vec) << std::endl;
 };
 
+// Info #2: When using lambdas, you can also use defaulted and delete
+// members to prevent certain usage or specify user defined
+// interactions.
+struct generic_sum
+{
+    template<typename T1, typename T2>
+    auto operator()(T1 const t1, T2 const t2) const { return t1 + t2; }
+    generic_sum(const generic_sum&) = default;
+    generic_sum(generic_sum&&) = default;
+    generic_sum& operator=(const generic_sum&) = delete;
+    ~generic_sum() = default;
+};
 
 int main(int argc, char** argv)
 {
@@ -62,6 +79,47 @@ int main(int argc, char** argv)
     std::cout << "For the string vector: " << std::endl;
     for( auto str : my_texts) { std:: cout << str << ", "; }
     std::cout << std::endl << "Concatenation: " << concatenate << std::endl;
+
+    // Info #: Using a template for std::vectors lambdas with templates.
+    std::cout << "TEMPLATE LAMBDA\nFor the numeric vector: " << std::endl;
+    for( auto num : my_nums) { std:: cout << num << ", "; }
+    vector_lambda(my_nums);
+    //vector_lambda(1620); // Will fail because it is not vector.
+
+    // Info #: Using generic lambdas to create templates with two different
+    // variables of the same type.
+    auto displayer = []<typename T>(T x, T y)
+    {
+        std::cout << "Element 1: " << x << "\tElement 2: " << y << std::endl;
+    };
+    displayer(16, 20);
+    displayer("Hi", "Dan");
+    // displayer(10, "Dan"); // Will launch error
+
+
+    // Info #: There may be cases when you need to know the type of a 
+    // parameter and then you can use the instances and the static methods,
+    // then you can use type deducers and pointers.
+
+    // Define a struct with a static member.
+    struct str_member
+    {
+        static void disp() { std::cout << "Str member" << std::endl; }
+    };
+
+    auto type_det = [](auto x)
+    {
+        // Determindate type of X and then use it with T
+        using T = std::decay_t<decltype(x)>;
+
+        // After identification, create type and call method disp.
+        T other;
+        T::disp();
+    };
+
+    // Call lambda
+    type_det(str_member{});
+
 
     return 0;
 }
