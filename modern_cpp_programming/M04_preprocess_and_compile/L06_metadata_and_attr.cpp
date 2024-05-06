d57f584cb02ab4b76b15b453bd18f8c781cbf6aa
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-05-06 08:30:33 -0500

    Modern C++ M04: attributes for no discard and deprecation

--- modern_cpp_programming/M04_preprocess_and_compile/L06_metadata_and_attr.cpp
+++ modern_cpp_programming/M04_preprocess_and_compile/L06_metadata_and_attr.cpp
@@ -13,14 +13,59 @@
 // Info #1: If you want to make sure a return value isn't discarded, you can use
 // [[nodiscard]] attribut. Or even, [[nodiscard(text)]] after C++20 when it comes
 // to use a message for better understanding.
-
 [[nodiscard]] int get_letter()
 {
     return 'd';
 }
 
+// Info #2: Another usage of [[nodiscard]] is related with the case of do not
+// ignore certain types of classes or enumerations.
+enum class [[nodiscard]] ErrorCode { Success, FileNotFound, InvalidInput };
+ErrorCode processRead(int input)
+{
+    if (input < 0)
+    {
+        std::cout << "Error recieved: Invalid Key..." << std::endl;
+        return ErrorCode::InvalidInput;
+    }
+    return ErrorCode::Success;
+}
+
+// Info #3: Have you seen a code get [deprecated]? Well, now you can use the
+// attribute and implement it (wisely) in your code when the functionalities
+// are going to change in the future. It can be applied to funcs and classes.
+[[deprecated("Use another getter")]] int old_getter()
+{
+    return 0;
+}
+
+
 int main(int argc, char* argv[])
 {
-    get_letter();
+    // No discard instrucction
+    std::cout << std::endl << "NO DISCARD ATTRIBUTE: " << std::endl
+              << "If you discard the result of the getter, you will have a "
+              << " warning, do not forget to experiment with the code."
+              << std::endl;
+    auto letter = get_letter();
+    // get_letter(); // Will return: Ignoring return value declared with
+                     // no discard.
+
+    // Usage of certain variables with no discard instruction
+    std::cout << std::endl << "ENUMS AND NO DISCARD ATTRIBUTE: " << std::endl
+              << "Here the no-dicard is attached directly to the class,"
+              << " and it can serve a similar usage to the ones in funcs but"
+              << " with results that can be expanded to all the escenarios."
+              << "of the class. Now watch, the invalid key case no discarded..." 
+              << std::endl;
+    auto status = processRead(-1);
+    // processRead(-1);
+
+    // Warning of deprecation
+    std::cout << std::endl << "DEPRECATION: " << std::endl
+              << "Uncomment the function below to watch a warn of a old func"
+              << std::endl;
+    auto old_num = old_getter();
+
     return 0;
 }
