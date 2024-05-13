3247e41d821f042dd2706653a8eb588a37ffba26
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-05-13 10:35:39 -0500

    Modern C++ M01: More on uniform initialization with more types

--- modern_cpp_programming/M01_core_modern_features/L03_uniform_initialze.cpp
+++ modern_cpp_programming/M01_core_modern_features/L03_uniform_initialze.cpp
@@ -8,12 +8,26 @@
  * called uniform initialization. You will check some comparation thoughout
  * this code for what a proper initialization should be and how can they be
  * improved.
+ * 
+ * Down below you will watch examples using braces for uniform initalization
+ * in different situations and with different types.
+ * 
+ * NOTE: If you do not know about POD, it comes from Plain Old Data, it refers
+ * to trivial and standard layout (defaulted members by compiler). But after 
+ * C++20 it is a deprecated concept in order to prefer the latter concepts 
+ * (trivial and standard).
+ * 
+ * Also, keep in mind that you cannot initialize integers or floats directly
+ * with braces. And it also happens with narrowing conversions like long double
+ * to float or doubles, or unscoped enumerations. 
 */
+
+// --------------------------- INCLUDES AND DEPENDENCIES ----------------------
 
 #include <vector>
 #include <map>
 
-// Custom types and classes defined for this lesson
+// --------------------------- CUSTOMS DEFINITION -----------------------------
 class question
 {
 private:
@@ -26,11 +40,16 @@ public:
         :q_text_(q_text), correct_opt_(correct_opt) 
     {   std::cout << "Question: User constructor" << std::endl; }
 };
+
 struct rectangle 
 {
     float l1; 
     float l2;
 };
+
+void display_content(std::initializer_list<float> const);
+
+// ----------------------------- MAIN IMPLEMENTATION -------------------------
 
 int main(int argc, char* argv[])
 {
@@ -52,6 +71,7 @@ int main(int argc, char* argv[])
     std::map<int, char> idx_abc { {1, 'a'}, {2, 'b'}, {3, 'c'}};
     float* my_float_vector = new float[2]{1.6, 2.0};
     char my_char_array[3]{'D', 'a', 'n'};
+    // Let's display their content...
     std::cout << "Uniform init experiments: " << std::endl << "\tInt Vector:";
     for( auto num : my_int_vector)
     {
@@ -62,9 +82,43 @@ int main(int argc, char* argv[])
     {
         std::cout << "(" << pair.first << ", " << pair.second << "), ";
     }
-    std::cout << std::endl;
-
-
+    std::cout << std::endl << "Float Array: ";
+    for(int i = 0; i < 2; i++)
+    {
+        std::cout << my_float_vector[i] << ", ";
+    } 
+    std::cout << std::endl << "Char Array";
+    for(int j = 0; j < 3; j++)
+    {
+        std::cout << my_char_array[j] << ", ";
+    }
+    std::cout << std::endl << "Now let's watch for classes" << std::endl;
+    question first_q{};
+    question second_q{"Name?", 'D'};
+    rectangle rect{1.6, 6.2};
+    std::cout << "POD Type Rectangle: (" << rect.l1 << ", " << rect.l2
+              << ")" << std::endl << std::endl;
+    
+    // Info #3: You can use std::initialize_list<T> for initialization of
+    // standard containers, and it can also be used to overload functions
+    // or methods to suppport more arguments.
+    std::cout << "Initialization with list: " << std::endl;
+    display_content({16, 20, 62});
 
     return 0;
+}
+
+/**
+ * A simple function to display the content list passed.
+ * 
+ * @param list A collection of float elements.
+*/
+void display_content(std::initializer_list<float> const list)
+{
+    std::cout << "( ";
+    for(auto const & element : list)
+    {
+        std:: cout << element << ", ";
+    }
+    std::cout << " )" << std::endl; 
 }
