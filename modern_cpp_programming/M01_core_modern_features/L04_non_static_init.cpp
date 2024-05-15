2e065abc82b9fdf51e10968adc9c86c0713b9019
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-05-15 09:03:34 -0500

    Modern C++ M01: Non static member initialization

--- modern_cpp_programming/M01_core_modern_features/L04_non_static_init.cpp
+++ modern_cpp_programming/M01_core_modern_features/L04_non_static_init.cpp
@@ -10,6 +10,21 @@
  * initialization, but keep in mind that you have to use assignments in
  * constructos when the options presented aren't possible.
  * 
+ * Using assignments in constructor isn't efficient as it creates copies 
+ * (temporals that are later discarded). Also, if no initilizer is provided in
+ * the construct, it will use the default definitions present in the body.
+ * 
+ * For this process, consider that the non-static members are initialized in the
+ * order they were decalred (not the order provided in the constructor), the
+ * reverse case happens with destructors. 
+ * 
+ * So, when using built-int types, you should prefer constructor initializer
+ * list. But, keep consisten with your initializations, as there are cases where
+ * you cannot use a constructor list initializer:
+ * 
+ * - In cases that the member has been initialized with pointer 'this'.
+ * - Elements with references to each other (complementaries).
+ * - Test of inputs params for checking exceptions.
 */
 
 #include <string>
@@ -36,11 +51,17 @@ struct BasicStruct
 
     // Info #3: Constructor initialization for members that don't have default
     // values, but depend on parameters.
-    BasicStruct(std::string const & name) : name(name) {}
+    BasicStruct(std::string const & name) : name(name) 
+    {
+        std::cout << "BasicStruct -> Name user constructor" << std::endl;
+    }
     BasicStruct(std::string const & name,
                  PositionNum const pn,
                  PositionLetter const pl):
-                 name(name), num(pn), letter(pl) {}
+                 name(name), num(pn), letter(pl) 
+    {
+        std::cout << "BasicStruct -> Full user constructor" << std::endl;
+    }
 };
 
 // Info #4: A simple struct with non-static data members initialization in the
@@ -53,11 +74,51 @@ struct Label
 
     // Initializer
     Label(std::string const name = " ", int const ID = 0) : name(name), ID(ID)
-    {}
+    {
+        std::cout << "Label -> User constructor" << std::endl;
+    }
+};
+
+// Info #5: In case you want to understand better the constructor options, take
+// a look to the class below.
+struct constructor_check
+{
+    constructor_check() 
+    { std::cout << "CC -> Default constructor" << std::endl; }
+    constructor_check(std::string const & text) 
+    { std::cout << "CC -> Constructor " << text << std::endl; }
+    constructor_check(constructor_check const & other) 
+    { std::cout << "CC -> Copy constructor" << std::endl; }
+    constructor_check(constructor_check&& other) 
+    { std::cout << "CC -> Move constructor" << std::endl; }
+    constructor_check& operator=(constructor_check const & other) 
+    { std::cout << "CC -> Assignment" << std::endl; }
+    constructor_check& operator=(constructor_check&& other)
+    { std::cout << "CC -> Move assignment" << std::endl; }
+    ~constructor_check()
+    { std::cout << "CC -> Destructor" << std::endl; }
 }
+
+// --------------------------- MAIN IMPLEMENTATION ----------------------------
 
 int main(int argc, char* argv[])
 {
+    // Using our basic structure and the non static initialization
+    std::cout << "Check the different constructor usage for BasicStruct: "
+              << std::endl;
+    auto basic1{ BasicStruct("Dan") };
+    auto basic2{ BasicStruct("Dan", PositionNum::Three, PositionLetter::C3) }; 
+
+    // Using Label non-static initialization
+    std::cout << std::endl << "Now, let's check the case with label: " 
+              << std::endl;
+    auto label1 { Label("Dan", 16) };
+
+    // Checking constructor usage
+    constructor_check ex1;
+    constructor_check ex2{ "Hi!" };
+    constructor_check ex3{ ex2 };
+    constructor_check ex4 = ex3;
 
     return 0;
 }
