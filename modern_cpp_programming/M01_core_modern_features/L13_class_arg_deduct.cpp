2ebef2a6bef5d6f5530d17f8833ef3ce2fd723f4
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-06-05 11:59:08 -0500

    Modern C++ M01: Using inline namespaces

---
+++ modern_cpp_programming/M01_core_modern_features/L13_class_arg_deduct.cpp
@@ -0,0 +1,14 @@
+// BASED ON THE "MODERN C++  PROGRAMMING COOKBOOK - 2 EDITION"
+// Code was tested with g++ in C++20
+
+#include <iostream>
+
+/**
+ * 
+*/
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
