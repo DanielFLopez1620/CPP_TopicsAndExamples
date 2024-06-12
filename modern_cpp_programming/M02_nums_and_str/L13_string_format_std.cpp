b9a60f1689afb1de02b702ea862e8c414312dc04
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-06-12 07:49:58 -0500

    Modern C++ M02: Minor corrections and format on module 2 P3

--- modern_cpp_programming/M02_nums_and_str/L13_string_format_std.cpp
+++ modern_cpp_programming/M02_nums_and_str/L13_string_format_std.cpp
@@ -1,5 +1,5 @@
 // BASED ON THE "MODERN C++  PROGRAMMING COOKBOOK - 2 EDITION"
-// Code was not testedm but must be run with g++ in C++20
+// Code was tested with C++20 with g++ Version 13
 
 #include <iostream>
 
@@ -19,49 +19,64 @@
  *  https://en.cppreference.com/w/cpp/chrono/system_clock/formatter
  * 
  * You can compile this code with:
- *      g++ -std=c++20 L13_string_format_std.cpp -o format.out
+ *      g++-13 -std=c++20 L13_string_format_std.cpp -o format.out
  *      ./format.out
+ * 
+ * For this code, you may have to update GCC manually, by installing:
+ * 
+ *      sudo apt install g++-13
 */
 
-// #include <fmt/core.h>
-#include <format>
-#include <chrono>
-#include <vector>
 
+// -------------------- REQUIRED HEADERS --------------------------------------
+#include <format>   // Library for a new format option for str
+#include <chrono>   // Time management in different precisions
+#include <vector>   // For dynamic arrays
+#include <ctime>    // For standard time with C Library
+#include <iterator> // For iteration of elements
+#include <string>   // For word and str formatting 
+
+// ------------------- FUNCTION PROTOTYPES ------------------------------------
+std::string format_time(const std::tm& tm);
+
+// -------------------- MAIN FUNCTION -----------------------------------------
 int main(int argc, char** argv)
 {
+    std::cout << "\nLesson 13: Using std::format....\n" << std::endl;
+
     // Info #1: Providing arguments in new format.
     auto text1 = std::format("My name is {} {} ", "Dan", 1620);
-    std::cout << text1 << std::endl;
+    std::cout << "\nFormatted output: " << text1 << std::endl;
     
     // Info #2: Providing arguments with order.
     auto text2 = std::format("My name is {1} {0}", "Dan", 1620);
+    std::cout << "\nChanged order of params: " << text2 << std::endl;
 
     // Info #3: You can specify format of numbers with ':'.
     auto num_hex = std::format("{0} equivalent hex is {0:08X}", 16);
-    std::cout << "Number: " << hum_hex << std::endl;
+    std::cout << "\nPrinting a number: " << num_hex << std::endl;
 
     // Info #4: You can specify time display properties related with chronos.
-    auto now = std::chrono::sysmet_clock::now();
+    auto now = std::chrono::system_clock::now();
     auto time_got = std::chrono::system_clock::to_time_t(now);
-    auto time_str = std::format("Date: {:{%Y-%m-%d}}", 
-        *std::localtime(&time_got));
+    auto time_str = std::format("Date: {}", format_time(*std::localtime(&time_got)));
+    std::cout << "\nPrinting chrono time: " << time_str << std::endl;
 
     // Info #5: Writing buffers from std::format to std::string.
     std::vector<char> buffer;
     std::format_to(std::back_inserter(buffer), "{} + {}", "Dan", 1620);
+    std::cout << "\nUsing buffer: " 
+              << std::string(buffer.data(), buffer.size()) << std::endl;
 
-    // Info #6: Considering the size of std::format for buffers.
-    auto text3 = std::format("{}{} is playing.", "Dan", 1620);
-    auto size = std::formatted_size(text3);
-    std::vector<char> buffer2(size);
-    std::format_to(size.data(), text3);
+    // Info #6: Considering the size of std::format for buffers and prevent
+    // extra characters in buffer.
+    auto text3 = std::format("{} {} is playing.", "Dan", 1620);
+    std::vector<char> buffer2(text3.size());
+    std::format_to(buffer2.data(),"{}", text3);
+    std::cout << "\nUsing another buffer for format: "
+              << std::string(buffer2.data(), buffer2.size()) << std::endl;
 
-    // Info #7: Prevent extra characters in buffers:
-    char buffer3[100];
-    auto result = std::format_to_n(buffer3, sizeof(buffer3), text3);
-
-    // Info #8: Using format options to fill with characters.
+    // Info #7: Using format options to fill with characters.
     //  < : Forces left align
     //  > : Forces right align
     //  ^ : Centered according available spaces.
@@ -93,7 +108,8 @@ int main(int argc, char** argv)
     auto txt5 = std::format("{0:+}", 16);
     auto txt6 = std::format("{:#05x}", 16);
 
-    std::cout << "Numeric align: " << txt1 << std::endl
+    std::cout << "\nFinally, using align with format:" << std::endl
+              << "Numeric align: " << txt1 << std::endl
               << "Text left align: " << txt2 << std::endl
               << "Text right align: " << txt3 << std::endl
               << "Centered align: " << txt4 << std::endl
@@ -102,7 +118,22 @@ int main(int argc, char** argv)
 
 
     return 0;
-}
+} // main
 
+// ---------------------- FUNCTION DEFINITIONS --------------------------------
 
+/**
+ * For formatting time tm structs
+ * 
+ * @param tm Time struct to consider
+ * 
+ * @return Char buffer for printing after conversion
+ */
+std::string format_time(const std::tm& tm)
+{
+    char buffer[100];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d", &tm);
+    return buffer;
+
+} // format_time
 
