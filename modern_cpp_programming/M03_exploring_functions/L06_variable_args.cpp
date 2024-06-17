50b86c92f77a0ccfdfaa745c71b5f882e2736b67
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-06-17 07:38:57 -0500

    Modern C++ M03: Minot corrections P2 and format on module 3 P3

--- modern_cpp_programming/M03_exploring_functions/L06_variable_args.cpp
+++ modern_cpp_programming/M03_exploring_functions/L06_variable_args.cpp
@@ -1,5 +1,5 @@
 // BASED ON THE "MODERN C++  PROGRAMMING COOKBOOK - 2 EDITION"
-// Code wasn't tested, oriented with g++ in C++20
+// Code was tested with C++ 20 using G++
 
 #include <iostream>
 
@@ -29,46 +29,90 @@
  *      ./var_args.out
 */
 
-#include <vector>
+// ---------------------- REQUIRED HEADERS ------------------------------------
+#include <vector>         // For using different types of dynamic arrays.
+#include <type_traits>    // For given policy and implementation details.
+
+// -------------------- STRUCT AND CLASS DEFINITIONS --------------------------
 
 // Info #1: Implementing the steps to having variable arguments, you may notice
-// it seems like a recursion, but to be mroe precise it is a compile time
+// it seems like a recursion, but to be more precise it is a compile time
 // recursion rather than a runtime recursion. This makes the compiler generate
 // multiple functions with a different nums of arguments. Also keep in mind:
 // - typename... Ts : Is a template parameter packt.
 // - typename... others : Is a function parameter pack for function with variable
 //   number of arguments.
 // - others... : Is a expansion of the parameter packt
+
+/**
+ * Generic implementation for a multiplication with just one value, in other 
+ * words, multiplication of the given number by one.
+ * 
+ * @param value Number to consider in the multiplication
+ * 
+ * @return Product of the value by 1
+ */
 template <typename T>
 T factors(T value)
 {
     return value;
-}
+
+} // factors
+
+/**
+ * Generic implementation of a multiplication that considers a pack of 
+ * arguments for a consecutive multiplication. By having as a root the
+ * default 1 product defined previously.
+ * 
+ * @param first First factor to consider
+ * @param others Pack of arguments for the multiplication
+ * 
+ * @return Consecutive product of the args passed.
+ */
 template<typename T, typename... Ts>
 T factors(T first, Ts... others)
 {
     // std::cout << __PRETTY_FUNCTION__ << std::endl;
     return first * factors(others...);
-}
+
+} // factors
 
 // Info #2: Not every variable args implementation need a sort of recursion, it
 // can be used also made by knowing the total size of the args.
+/**
+ * Function with variadic arguments implementation for generating a vector with
+ * just odd numbers.
+ * 
+ * @param nums Pack of numbers to consider
+ * 
+ * @return Vector formed with the pack of numbers passed.
+ * 
+ * @exception Static assert if one of the provided args isnt' an odd number.
+ */
 template<typename... T>
 auto make_odd_vector(T... nums)
 {
     static_assert(sizeof...(nums)%2 != 0, "Error: expected odd number");
-    std::vector<T...> vect {nums... };
+    using common_type = std::common_type_t<T...>;
+    std::vector<common_type> vect {nums...};
     return vect;
-}
+
+} // make_odd_vector
+
+// ---------------------------- MAIN IMPLEMENTATION ---------------------------
 
 int main(int argc, char** argv)
 {
+    std::cout << "Lesson 6: Understaing variadic arguments for funcs...\n"
+              << std::endl;
+
     // Info #3: Using variable args from multiple types.
     auto product1 = factors(16, 20, 30);
     auto product2 = factors(1.6, 2.0, 3.0, 5.2);
 
-    std::cout << "Multiplication 1: " << product1 << std::endl
-              << "Multiplication 2: " << product2 << std::endl << std::endl;
+    std::cout << "Multiplication 1 (16, 20, 30): " << product1 << std::endl
+              << "Multiplication 2 (1.6, 2.0, 3.0, 5.2): " << product2 
+              << std::endl << std::endl;
 
     // Info #4: Using odd vector implementation
     auto num1 = 1, num2 = 7, num3 = 5;
@@ -77,4 +121,5 @@ int main(int argc, char** argv)
     auto vector1 = make_odd_vector(num1, num2, num3);
     
     return 0;
-}
+
+} // main
--- modern_cpp_programming/M03_exploring_functions/L07_fold_expressions.cpp
+++ modern_cpp_programming/M03_exploring_functions/L07_fold_expressions.cpp
@@ -4,9 +4,9 @@
 #include <iostream>
 
 /**
- * Fold expressions appeared in C++17, they allo to reduce a parameter pack over
+ * Fold expressions appeared in C++17, they allow to reduce a parameter pack over
  * binary operators, then it simplifies variadic templates to make them more
- * concise and readble.
+ * concise and readable.
  * 
  * The fold operations are defined inside parenthesis and have a parapmeter pack,
  * one or two operators, and a ellipsis (...). Some examples of the syntax are:
@@ -29,76 +29,153 @@
  * 
 */
 
+// ------------------------ STRUCT AND CLASS DEFINITIONS ----------------------
+
 // Info #1: As you may remember, the correct form for implementing variable
-// nums of args, is by using templates with a pack:
+// nums of args, is by using templates with a pack (and also having a frist
+// implementation with just one arg):
+/**
+ * Generic implementation for a multiplication with just one value, in other 
+ * words, multiplication of the given number by one.
+ * 
+ * @param value Number to consider in the multiplication
+ * 
+ * @return Product of the value by 1
+ */
 template <typename T>
 T products1(T value)
 {
     return value;
-}
+
+} // products1
+
+/**
+ * Generic implementation of a multiplication that considers a pack of 
+ * arguments for a consecutive multiplication. By having as a root the
+ * default 1 product defined previously.
+ * 
+ * @param first First factor to consider
+ * @param others Pack of arguments for the multiplication
+ * 
+ * @return Consecutive product of the args passed.
+ */
 template <typename T, typename... Ts>
 T products1(T begin, Ts... others)
 {
     return begin * products1(others...);
-}
+
+} // products1
 
 // Info #2: Let's use examples of folding expressions as they where previously
 // mentioned and classified:
 
-//  - Unary left folding:
+//  - UNARY LEFT FOLDING:
+/**
+ * Generic implementation of a multiplication that considers a pack of 
+ * arguments for a consecutive multiplication. By implementing a unary
+ * left folding.
+ * 
+ * @param args Pack of numeric arguments to consider.
+ * 
+ * @return Consecutive product of the args passed.
+ */
 template <typename... Ts>
 auto products2(Ts... args)
 {
     return (... * args);
-}
+} // products2
 
-//  - Binary left folding:
+//  - BINARY LEFT FOLDING:
+/**
+ * Generic implementation of a multiplication that considers a pack of 
+ * arguments for a consecutive multiplication. By implementing a binary
+ * left folding.
+ * 
+ * NOTE: This add a 2 factor, for obtaining a duplicated result.
+ * 
+ * @param args Pack of numeric arguments to consider.
+ * 
+ * @return Duplicate of the consecutive product of the args passed. 
+ */
 template <typename... Ts>
 auto duplicate_product1(Ts... args)
 {
     return (2 * ... * args);
-}
+} // duplicate_product1
 
-//  - Unary right folding:
+//  - UNARY RIGHT FOLDING:
+/**
+ * Generic implementation of a multiplication that considers a pack of 
+ * arguments for a consecutive multiplication. By implementing a unary
+ * right folding.
+ * 
+ * @param args Pack of numeric arguments to consider.
+ * 
+ * @return Consecutive product of the args passed.
+ */
 template <typename... Ts>
 auto products3(Ts... args)
 {
     return (args + ...);
-}
+}  //product3
 
-//  - Binary right folding:
+//  - BINARY RIGHT FOLDING:
+/**
+ * Generic implementation of a multiplication that considers a pack of 
+ * arguments for a consecutive multiplication. By implementing a binary
+ * right folding.
+ * 
+ * NOTE: This add a 2 factor, for obtaining a duplicated result.
+ * 
+ * @param args Pack of numeric arguments to consider.
+ * 
+ * @return Duplicate of the consecutive product of the args passed. 
+ */
 template <typename... Ts>
 auto duplicate_product2(Ts... args)
 {
     return (args * ... * 2);
-}
+} // duplicate_product2
 
 // Info #3: Even though fold expressions work with overloads of supported
 // binary operators, it doesn't work with arbitrary binary functions. For
 // example, implementing a wrapper that holds a reference to a constant value,
 // it can also be used then for implementing a max value by considering the 
 // operator>
+/**
+ * Simple generic wrapper implementation
+ */
 template <typename T>
 struct wrapper
 {
     T const & value; 
-};
+}; // struct wrapper
 
+/**
+ * Comparison operator for the wrapper
+ */
 template <typename T>
 constexpr auto operator>(wrapper<T>  const lhs, wrapper<T> const & rhs)
 {
     return wrapper<T> {lhs.value > rhs.value ? lhs.value : rhs.value};
-}
+} // rappwer operator> 
 
+/**
+ * Max search value by considering a pack of argument that are treated
+ * with the generic wrapper implemented.
+ */
 template <typename... Ts>
 constexpr auto max(Ts&&... args)
 {
     return(wrapper<Ts>{args} > ...).value;
-}
 
+}  // max
 
+// ------------------------------- MAIN IMPLEMENTATION ------------------------
 int main(int argc, char** argv)
 {
+    std::cout << "Lesson 7: Fold expressions for variadic arguments...\n"
+              << std::endl;
     // Info #4: Implementing folds products
     auto product1 = products1(16, 20, 30);
     auto product2 = products2(1.6, 2.0, 3.0, 5.2);
@@ -117,4 +194,5 @@ int main(int argc, char** argv)
               << std::endl;
 
     return 0;
-}
+
+} // main
