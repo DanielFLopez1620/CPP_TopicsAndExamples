1fbc335f0f10214dca0efaae611dc47b29b5edb4
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-06-27 10:36:00 -0500

    Modern C++ M05: More methods with bitset sequences

--- modern_cpp_programming/M05_more_on_std_lib/L02_bitset_sequence.cpp
+++ modern_cpp_programming/M05_more_on_std_lib/L02_bitset_sequence.cpp
@@ -12,6 +12,25 @@
  * That is the reason, the std::bitset was provided as a container for fixed-size
  * sequences of bits. It is present in the header <bitset> and allow fixed-size
  * sequences of bits.
+ * 
+ * In case you haven't worked with bit sets, they can be used for:
+ * 
+ *  - Define bit flags for style, options and more (even with macros).
+ *  - Easy change of fligs by using operator OR or negated AND flag.
+ *  - Checking the correspondy between flags (value & FLAG == FLAG)
+ * 
+ * With the implemantion of bitset class, the aim was set to write more robust
+ * and safer code, by considering the new methods of set() and reset(), use
+ * a proper check with test() and allow valid conversions to string and numeric
+ * types.
+ * 
+ * In case, you cannot use bitset, there is an option called std::Vector<bool>
+ * that you will check on the next lesson.
+ * 
+ * When you are ready, you can compile an run this code with:
+ * 
+ *      g++ -std?c++20 L02_bitset_sequence.cpp -o bitset.out
+ *      ./bitset.out 
  */
 
 #include <bitset>
@@ -41,6 +60,33 @@ int main(int argc, char* argv[])
               << "Count of ones: " << bset2.count() << std::endl
               << "Contians any ones?: " << bset2.any() << std::endl
               << "Is everything one?: " << bset2.all() << std::endl
-              << "Is everything zero?: " << bset2.none() << std::endl;
+              << "Is everything zero?: " << bset2.none() << std::endl
+              << "Is it event (test)?: " << bset2.test(0) << std::endl;
+
+    // Info #3: You can perform binary operations between among bitsets or
+    // one bitset with itself. They can be OR, AND, XOR and NOT bitwise or
+    // even shifting operations (left or right), let's check some examples:
+    std::cout << "\nLet's use the bitset 3 and 4 for some binary operations: "
+              << std::endl << "B3: " << bset3 << ", B4: " << bset4
+              << std::endl << "\tb3 | b4: " << (bset3 | bset4)
+              << std::endl << "\tb3 & b4: " << (bset3 & bset4)
+              << std::endl << "\tb3 ^ b4: " << (bset3 ^ bset4)
+              << std::endl << "\t~b3" << (~bset3)
+              << std::endl << "\tb3 >> 2: " << (bset3 >> 2)
+              << std::endl << "\tb3 << 2: " << (bset3 << 2)
+              << std::endl << "\tflip(b3): " << (bset3.flip())
+              << std::endl;
+    
+    // Info #4: In case yo need to convert a bit set to a value or a string, 
+    // C++ has proper methods to no the conversion. But be careful with the
+    // ones that convert to numerical values, as you may need to handle the
+    // std::overflow_error excception.
+    std::cout << "\nLet's convert the bitset2 to different types: "
+              << std::endl << "\tUnsigned long: " << bset2.to_ulong()
+              << std::endl << "\tUnsigned long long: " << bset2.to_ullong()
+              << std::endl << "\tString: " << bset2.to_string()
+              << std::endl << "\tString(x,o): " << bset2.to_string('o', 'x')
+              << std::endl;
+
     return 0;
 }
