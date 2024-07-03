cfc42dfcc8d70c73fe9cd2d5e3b38e739b1185ee
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-07-03 07:46:40 -0500

    Modern C++ M05: Some cases of bit manipulation

--- modern_cpp_programming/M05_more_on_std_lib/L04_bit_manipulation.cpp
+++ modern_cpp_programming/M05_more_on_std_lib/L04_bit_manipulation.cpp
@@ -28,7 +28,8 @@ int main(int argc, char* argv[])
               << "\tLeft Shift by 2: " << std::rotl(my_bin, 2) << std::endl
               << "\tLeft Shift by 4: " << std::rotl(my_bin, 4) << std::endl
               << "\tRight Shift by 1: " << std::rotr(my_bin, 1) << std::endl
-              << "\tRight Shift by 6: " << std::rotr(my_bin, 6) << std::endl;
+              << "\tRight Shift by 6: " << std::rotr(my_bin, 6) << std::endl
+              << std::endl;
     
     // Info #2: Another interesting method is to use a counter of how many
     // positions until a one is found, it can be 'countl_zero' for start at the
@@ -50,9 +51,38 @@ int main(int argc, char* argv[])
               << "\t\tC1 Right: " << std::countr_one(my_bin4) << std::endl
               << "\tBinary #5:" << my_bin5 << std::endl
               << "\t\tC1 left: " << std::countl_one(my_bin5) << std::endl
-              << "\t\tC1 Right: " << std::countr_one(my_bin5) << std::endl;
+              << "\t\tC1 Right: " << std::countr_one(my_bin5) << std::endl
+              << std::endl;
 
     
     // Info #3: You can also get the number of 1 active throught the element by
     // using popcount.
+    std::cout << "Counting 1s present in a binary: " << std::endl
+              << "\tBinary #1: " << my_bin << " -> " << std::popcount(my_bin) 
+              << std::endl << "\tBinary #2: " << my_bin2 << " -> "
+              << std::popcount(my_bin2) << std::endl << "\tBinary 3:" << my_bin3
+              << " -> " << std::popcount(my_bin3) << std::endl << "\tBinary 4:"
+              << my_bin4 << " -> " << std::popcount(my_bin4) << std::endl
+              << std::endl;
+
+    // Info #4: You can easily detect if a number or bit sequence is a power of
+    // two by comprobating if just one bit is active along the sequence, you can
+    // achieve it with 'has_single_bit()' method.
+    std::cout << "Checking if binaries are power of 2:" << std::endl
+              << "\tBinary #1: " << my_bin << " -> " 
+              << std::has_single_bit(my_bin) << std::endl << "\tBinary #2: "
+              << my_bin2 << " -> " << std::has_single_bit(my_bin2) << std::endl
+              << "\tBinary 3: " << my_bin3 << " -> " 
+              << std::has_single_bit(my_bin3) << std::endl << std::endl;
+
+    // Info #5: You can get how many bits are necesarry to represent 
+    // certain num, based on ceil, floor or width cases.
+    std::cout << "Checking binaries size for my_bin 4" << std::endl
+              << "\tOriginal binary: " << my_bin4 << std::endl
+              << "\tCeil size: " << std::bit_ceil(my_bin4) << std::endl
+              << "\tFloor size: " << std::bit_floor(my_bin4) << std::endl
+              << "\tWidth size: " << std::bit_width(my_bin4) << std::endl;
+
+    // Info #6: There is also options for casting types, for example, a double to its
+    // binary representation or hex.
 }
