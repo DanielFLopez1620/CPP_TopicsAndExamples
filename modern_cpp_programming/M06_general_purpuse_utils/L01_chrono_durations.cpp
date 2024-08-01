b6b1f37c623e3cf7e166e7a272a0c0f756c40c25
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-08-01 09:02:35 -0500

    Modern C++ M06: Using chrono durations P2

--- modern_cpp_programming/M06_general_purpuse_utils/L01_chrono_durations.cpp
+++ modern_cpp_programming/M06_general_purpuse_utils/L01_chrono_durations.cpp
@@ -1,5 +1,5 @@
 // BASED ON THE "MODERN C++  PROGRAMMING COOKBOOK - 2 EDITION"
-// Code was tested with g++ in C++17
+// Code was tested with g++ in C++20
 
 #include <iostream>
 
@@ -46,11 +46,53 @@ int main(int argc, char* argv[])
     auto quarter_of_second = 250ms;
     auto quarter_of_millisecond = 250us;
     std::cout << "Using chrono literals, but let's just display a count "
-              << std::endl << "\t" 
-              
-    // Info #3: You can proceed to make conversion from lower precision
-    // duration to a higher precision duration using .count() method.
+              << std::endl << "\t" << quarter_of_day << " : " << quarter_of_hour
+              << " : " << quarter_of_minute << " : " << quarter_of_second
+              << " : " << quarter_of_millisecond << std::endl;
 
+    // Info #3: You can proceed to make conversion from lower precision
+    // duration to a higher precision duration by using a duration definition
+    // cast, in other works, assigning a different duration a previously defined.
+    std::chrono::hours set_hours(16);
+    std::chrono::minutes set_hours_in_min(set_hours);
+    std::cout << "Converting from lower precision to higher: " << std::endl
+              << "\tTime in hours: " << set_hours << std::endl
+              << "\tSame time in minutes: " << set_hours_in_min << std::endl;
+
+    // Info #4: You can also make cast from higher precision to lower
+    // precision, but for this case you will need to use 
+    // std::chrono_duration_cast:
+    auto just_seconds = 1620162s;
+    auto just_seconds_in_h = std::chrono::duration_cast<std::chrono::hours>
+        (just_seconds);
+    std::cout << "Converting from higher precision to lower: " << std::endl
+              << "\tTime in seconds: " << just_seconds << std::endl
+              << "\tSame time in hours: " << just_seconds_in_h << std::endl;
+
+    // Info #5: When making cast, you usually do approximations, then you can
+    // use conversion functions like floor(), round() and ceil(), this was
+    // implemented in C+++17.
+    auto js_floor = std::chrono::floor<std::chrono::minutes>
+        (just_seconds);
+    auto js_ceil = std::chrono::ceil<std::chrono::minutes>
+        (just_seconds);
+    auto js_round = std::chrono::round<std::chrono::minutes>
+        (just_seconds);
+    std::cout << "Conversion considering approximations: " << std::endl
+              << "\tSeconds: " << just_seconds << std::endl
+              << "\tMinutes (Floor): " << js_floor << std::endl
+              << "\tMinutes (Ceil): " << js_ceil << std::endl
+              << "\tMinutes (Round): " << js_round << std::endl;
+    
+    // Info #6: You can also do arithmetic and comparison operations 
+    auto sum_thirds = third_of_hour + third_of_minute + third_of_second;
+    auto sum_of_quarters = quarter_of_minute + quarter_of_second 
+        + quarter_of_millisecond;
+    std::cout << "Making sums with different precisions: " << std::endl
+              << "\tSum of third of hour, minute and second: " << sum_thirds
+              << std::endl << "\tSum of quarter of minute, second and ms: "
+              << sum_of_quarters << std::endl;
+    
     return 0;
 }
 
