0ce0a38db4d4529d574bf0d7b1967245d8dfc56c
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-08-09 09:37:05 -0500

    Modern C++ M06: Using chrono calendars P3

--- modern_cpp_programming/M06_general_purpuse_utils/L02_calendars.cpp
+++ modern_cpp_programming/M06_general_purpuse_utils/L02_calendars.cpp
@@ -36,20 +36,85 @@ namespace std::chrono
     {
         return os << ymd.year() << '/' << ymd.month() << '/' << ymd.day();
     }
+
+    std::ostream& operator<<(std::ostream& os, const std::chrono::year_month_weekday& ymdw) 
+    {
+
+        os << int(ymdw.year()) << '/';
+        os << unsigned(ymdw.month()) << '/';
+
+        auto week_num = unsigned(ymdw.index());
+        auto day_num = unsigned(ymdw.weekday().iso_encoding());
+
+        // auto calendar_day = year_month_day { ymdw.year(), ymdw.month(), day{1}};
+        // auto real_date_days = calendar_day + day{day_num} + (week_num * day{7});
+        // os << unsigned(real_date_days.day());
+
+        switch (day_num)
+        {
+        case 0:
+            os << "[Sunday ";
+            break;
+        case 1:
+            os << "[Monday ";
+            break;
+        case 2:
+            os << "[Tuesday ";
+            break;
+        case 3:
+            os << "[Wednesday ";
+            break;
+        case 4:
+            os << "[Thurday ";
+            break;
+        case 5:
+            os << "[Friday ";
+            break;
+        case 6:
+            os << "[Saturday ";
+            break;
+        default:
+            os << " Error day ";
+            break;
+        };
+        os << " week " << week_num << " ]";
+
+        return os;
+    }
+
 }
+
 
 int main(int argc, char* argv[])
 {
     // Info #1: You can use Gregorian representation for different formats, by
-    // here you can consider using two forms of creation:
-    constexpr auto date_ymd_1 = std::chrono::year_month_day(2024y,
-        std::chrono::August, 7d);
+    // here you can consider using two forms of creation (you can use 
+    // namespaces to avoid long definitons, here they are used to tell you
+    // more about them in a proper way):
+    constexpr auto date_ymd_1 = 
+        std::chrono::year_month_day(2024y, std::chrono::August, 7d);
     const std::chrono::year_month_day date_ymd_2 = 2024y / 8 / 7;
-    std::cout << date_ymd_1 << std::endl << date_ymd_2 << std::endl;
+    std::cout << "Date definition with year_month_day type: " << std::endl
+              << "\tLong definition: "<< date_ymd_1 << std::endl 
+              << "\tShort denition: " << date_ymd_2 << std::endl;
 
     // Info #2: You can define the date in the format you want, like DMY, YMD
     // or MDY, and the chrono library will unified it:
+    std::chrono::year_month_day date_dmy = 8d / 8 / 2024;
+    std::chrono::year_month_day date_mdy = std::chrono::August / 8 / 2024;
+    std::chrono::year_month_day date_ymd = 2024y / 8 / 8;
+    std::cout << "Definitions of YMD, DMY and MDY with uniformity: "
+              << std::endl << "\tYMD format result: " << date_ymd
+              << std::endl << "\tMDY format result: " << date_mdy
+              << std::endl << "\tDMY format result: " << date_dmy
+              << std::endl;
 
+    // Info #3: Other possible definitons are related with 
+    // std::chrono::year_month_weekday in case you do not know the exact day
+    // but you have a specific day name in a certain week.
+    std::chrono::year_month_weekday tentative_date =
+        2024y / std::chrono::August / std::chrono::Wednesday[3];
+    std::cout << tentative_date << std::endl;
 
     return 0;
 }
