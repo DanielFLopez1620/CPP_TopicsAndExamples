a0b91ccdc49342bb87521082f75d87aa89fdc3ff
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-08-25 09:11:41 -0500

    Modern C++ M06: Using time zones with chrono P2

--- modern_cpp_programming/M06_general_purpuse_utils/L03_time_zones.cpp
+++ modern_cpp_programming/M06_general_purpuse_utils/L03_time_zones.cpp
@@ -14,19 +14,45 @@
  * - std::chrono::locate_zone(): Retrive a particular time zone.
  * - std::chrono::zoned_time(): Time point in a particular time zone.
  * 
+ * However, this may not be available for your compiler on C++20, for that 
+ * reason we will use the Howard Hinnant Date Library, which you can check on
+ * the notes.
+ * 
+ * To make sure we have it available, we will have to follow the next steps:
+ * 
+ *      cd <path_to_workspace>/CPP_TOPICSANDEXAMPLES/
+ *      cd modern_cpp_programming/M06_general_purpose_utils
+ *      git clone https://github.com/HowardHinnant/date.git
+ *      cd date
+ *      mkdir build
+ *      cd build
+ *      cmake ..
+ *      make
+ *      sudo make install
+ *      cmake -DBUILD_TZ_LIB=ON ..
+ *      make
+ *      sudo make install
+ * 
+ * To compile this code, you will need to run:
+ * 
+ *      g++ -std=c++20 -I/usr/local/include/ -L/usr/local/lib/ 
+ *          L03_time_zones.cpp -o L03_time_zones -ldate-tz -lcurl
+     
  * 
  * NOTE: Check https://howardhinnant.github.io/date/tz.html#Installation
  */
 
-#include<chrono>
+
+#include "date/tz.h"
+#include <chrono>
 
 int main(int argc, char *argv[])
 {
     std::cout << "Lesson 3: Time zones with chrono:\n" << std::endl;
 
     // Info #1: Obtaining current local time
-    auto cur_loc_time = std::chrono::zoned_time{ std::chrono::current_zone(), 
-        std::chrono::system_clock::now() };
+    auto now = std::chrono::system_clock::now();
+    auto cur_loc_time = date::make_zoned(date::current_zone(), now);
     std::cout << cur_loc_time << std::endl;
 
     return 0;
