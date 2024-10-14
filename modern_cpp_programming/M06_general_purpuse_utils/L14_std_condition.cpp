a305cb22af2c0d3c880a3b5b0dfde6a2e9e25545
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-10-14 09:12:42 -0500

    Modern C++ M06: Using std::condition for creating type selections in compile time P2

--- modern_cpp_programming/M06_general_purpuse_utils/L14_std_condition.cpp
+++ modern_cpp_programming/M06_general_purpuse_utils/L14_std_condition.cpp
@@ -9,12 +9,25 @@
  * transformation feature that is std::conditional which enables a way
  * two select/choose between two types at compilation time.
  * 
- *
+ * Check down below to understand the implementation of std::conditional_t
+ * to select among types according size, number of bytes or type for an
+ * array.
+ * 
+ * When ready, you can use:
+ *      
+ *      g++ -std=c++20 L14_std_condition.cpp -o condition.out
+ *      ./condition.out
  */
 
-#include <type_traits>
-#include <typeinfo>
+// --------------------- REQUIRED LIBRARIES -----------------------------------
+#include <type_traits>  // For info (in compile-time) about types
+#include <typeinfo>     // Contains info of the types, like name and unique id
+#include <random>       // For pseudo-random generations
 
+// --------------------- STRUCT AND TYPE DEFINITIONS --------------------------
+
+// Bit selector that will be used in INFO#2, allows for selection of size of
+// the integer in a proper way.
 template <int size>
     using num_bit_selector = 
         typename std::conditional_t<
@@ -24,9 +37,20 @@ template <int size>
                 typename std::conditional_t<
                     size<=4, std::int32_t, std::int64_t>>>;
 
+// ----------------------- FUNCTION PROTOTYPES -------------------------------
 template <typename T>
 void display_value_and_type(const T& value, std::string waited_precision);
 
+
+template <typename T,
+          typename D = std::conditional_t<
+              std::is_integral_v<T>, std::uniform_int_distribution<T>,
+              std::uniform_real_distribution<T>>,
+          typename = typename std::enable_if_t<
+              std::is_arithmetic_v<T>>>
+std::vector<T> RandomVector(T const min, T const max, size_t const size);
+
+// ------------------------ MAIN IMPLEMENTATION -------------------------------
 int main(int argc, char* argv[])
 {
     std::cout << "Lesson 14: Using std::conditional for deciding types\n"
@@ -61,24 +85,101 @@ int main(int argc, char* argv[])
     // Info #2: You can even generate nested std::conditional to generate
     // a more complex decision. In this case experimenting with size of
     // integers that go from int8 to int32.
-    num_bit_selector<1> var1 = 127;  // Maximum value for int8_t
+    num_bit_selector<1> var1 = 127;  // Max for int8_t
     display_value_and_type(var1, "int8");
-    num_bit_selector<2> var2 = 32000;  // Typical value for int16_t
+    num_bit_selector<2> var2 = 32000;  // Max for int16_t
     display_value_and_type(var2, "int16");
-    num_bit_selector<4> var3 = 2147483647;  // Maximum value for int32_t
+    num_bit_selector<4> var3 = 2147483647;  // Max for int32_t
     display_value_and_type(var3, "int32");
-    num_bit_selector<8> var4 = 9223372036854775807LL;  // Maximum value for int64_t
+    num_bit_selector<8> var4 = 9223372036854775807LL;  // Max for int64_t
     display_value_and_type(var4, "int32");
 
+    // Info #3: You can even use the std::conditional with containers, for
+    // example,  to generate a vector with random numbers.
+    auto random_vector = RandomVector(1.6, 16.0, 5);
+    std::cout << "Creating a random vector: \n\t( ";
+    for (auto const & element : random_vector)
+    {
+        std::cout << element << ", ";
+    }
+    std::cout << ")" << std::endl;
     return 0;
-}
 
+} // main()
+
+
+// -------------------------- FUNCTION DEFINITIONS ----------------------------
+
+/**
+ * Oriented to display info about the type selected by std::condition_t
+ * for the num_bit_selector_type.
+ * 
+ * @param value Inferenced value for the current type
+ * @param waited_precision Will log the waited precsiion by the user
+ */
 template <typename T>
 void display_value_and_type(const T& value, std::string waited_precision)
 {
+    auto my_int_type = typeid(T).name();
+    using Tt = std::remove_cv_t<std::remove_reference_t<T>>;
+    std::string my_int_type_name = "Unknown type";
+    if(std::is_same<Tt, int8_t>::value)
+    {
+        my_int_type_name = "int8";
+    }
+    else if(std::is_same<Tt, int16_t>::value)
+    {
+        my_int_type_name = "int16";
+    }
+    else if(std::is_same<Tt, int32_t>::value)
+    {
+        my_int_type_name = "int32";
+    }
+    else if(std::is_same<Tt, int64_t>::value)
+    {
+        my_int_type_name = "int64";
+    }
+
+
     // Print the value and its type using typeid
     std::cout << "\tWaited precision: " << waited_precision << std::endl
               << "\t\tValue: " << static_cast<int64_t>(value) << std::endl
-              << "\t\tType: " << typeid(T).name() << std::endl;
-              // TODO: Add type name
-}
+              << "\t\tType: " << my_int_type << std::endl
+              << "\t\tType name: " << my_int_type_name << std::endl;
+
+} // display_value_and_type()
+
+/**
+ * Function that creates a pseudo random vector (based on uniform distribution)
+ * that can be integral o real, by considering the type of the args passed.
+ * 
+ * @param min Minimum value of the sequence, can be integer or real
+ * @param max Maximum value of the sequence, can be integer or real
+ * @param size Integer value of the size of the array
+ * 
+ * @return Vector of the given size with pseudo-random elements.
+ */
+template <typename T,
+          typename D = std::conditional_t<
+              std::is_integral_v<T>, std::uniform_int_distribution<T>,
+              std::uniform_real_distribution<T>>,
+          typename = typename std::enable_if_t<
+              std::is_arithmetic_v<T>>>
+std::vector<T> RandomVector(T const min, T const max, size_t const size)
+{
+    // Initialize vector size
+    std::vector<T> v(size);
+
+    // Set pseudo random generation
+    std::random_device rand_dev{};
+    std::mt19937 mt { rand_dev() };
+    D dist{min, max};
+
+    // Create a pseudo-random value for each position
+    std::generate(std::begin(v), std::end(v),
+        [&dist, &mt] {return dist(mt); });
+    
+    // Return final vector
+    return v;
+
+} // RandomVector()
