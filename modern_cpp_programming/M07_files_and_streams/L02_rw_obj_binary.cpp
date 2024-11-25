cfba3d144bd324bd38c6501eb85bf43796c2163a
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-11-25 14:36:22 -0500

    Modern C++ M07: Writing and reading obj data in binary files P1

--- modern_cpp_programming/M07_files_and_streams/L02_rw_obj_binary.cpp
+++ modern_cpp_programming/M07_files_and_streams/L02_rw_obj_binary.cpp
@@ -11,6 +11,11 @@
  * explicitly decide what to write or read, and how to do it. 
  */
 
+#include <vector>
+#include <fstream>
+
+bool write_nonpod(std::ofstream& out_file);
+bool read_nonpod(std::ifstream& in_file);
 
 class msg_to_file
 {
@@ -56,5 +61,66 @@ bool operator==(pod_msg_to_file const & f1, pod_msg_to_file const & f2)
 
 int main(int argc, char* arv[])
 {
+    std::vector<pod_msg_to_file> pod_content {
+        {16, '1', {'2', '0'}},
+        {20, '2', {'1', '6'}}};
+
+    // Info #1: To serailize/deserialize POD types that doesn't have pointers,
+    // you can use ofstream::write() and ifstream::read() as it was shown in
+    // the previuos lesson.
+    std::ifstream out_file1("L02_example.bin", std::ios::binary);
+    if(out_file1.is_open())
+    {
+        for(auto const & value : pod_content)
+        {
+            out_file1.write(reinterpret_cast<const char*>(&value), 
+                sizeof(value));
+        }
+        out_file1.close();
+    }
+    std::vector<pod_msg_to_file> pod_review;
+    std::ifstream in_file1("L02_example.bin", std::ios::binary);
+    if(in_file1.is_open())
+    {
+        while(true)
+        {
+            pod_msg_to_file current;
+            in_file1.read(reinterpret_cast<char*>(&current), sizeof(current));
+            if(in_file1.fail() || in_file1.eof())
+            {
+                break;
+            }
+            pod_review.push_back(current);
+        }
+        in_file1.close();
+    }
+
+    // Info #2: However, when you have non-POD types, you must explicitly write
+    // the value of the data members to a file. And in case of reading and
+    // deserialize, you have to read from the file to the data members in the
+    // same order. Check the write_nonpod and read_nonpod function implementations.
+
     return 0;
+}
+
+bool write_nonpod(std::ofstream& out_file) const
+{
+    out_file.write(reinterpret_cast<const char*>(&num), sizeof(num));
+    out_file.write(&priority, sizeof(priority));
+    auto size = static_cast<int>(msg.size());
+    out_file.write(reinterpret_cast<char*>(&size), sizeof(size));
+    out_file.write(msg.data(), msg.size());
+    return !out_file.fail();
+
+}
+
+bool read_nonpod(std::ifstream& in_file)
+{
+    in_file.read(reinterpret_cast<char*>(&i), sizeof(i));
+    in_file.read(&priority, sizeof(priority));
+    auto size {0};
+    in_file.read(reinterpret_cast<char*>(&size), sizeof(size));
+    msg.resize(size);
+    in_file.read(interpret_cast<char*>(&msg.front()), size);
+    return !in_file.fail();
 }
