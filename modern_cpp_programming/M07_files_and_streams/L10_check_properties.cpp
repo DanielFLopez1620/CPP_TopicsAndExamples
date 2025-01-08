ff0cb7ffda1300e4f4470066487310551f522a4a
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-01-08 16:47:43 -0500

    Modern C++ M07: More on properties of a file

--- modern_cpp_programming/M07_files_and_streams/L10_check_properties.cpp
+++ modern_cpp_programming/M07_files_and_streams/L10_check_properties.cpp
@@ -49,6 +49,69 @@ int main(int argc, char* argv[])
 
     // Info #4: You can check the size of a file with 'file_size()' member.
     auto size = my_machine::file_size(file_path, err);
-    std::cout << "\tSize of the file:" << size << std::endl;
+    std::cout << "\tSize of the file: " << size << "bytes." << std::endl;
+
+    // Info #5: You can check the number of hardlinks with 'hard_link_count()'
+    // member, if you do not know what is a hard link, do not worry, in simple
+    // words it is an additional name for an existing files.
+    std::cout << "\tNumber of hardlinks: ";
+    auto hardlinks = my_machine::hard_link_count(file_path, err);
+    if(hardlinks != static_cast<uintmax_t>(-1))
+    {
+        std::cout << hardlinks << std::endl; 
+    }
+    else
+    {
+        std::cout << "Error... " << err << std::endl; 
+    }
+
+    // Info #6: You can also check the property for the last time a file was
+    // modified (writing operation,, can include deletions).
+    auto last_modified = my_machine::last_write_time(file_path, err);
+    auto time = to_time_t(last_modified);
+    auto localtime = std::localtime(&time);
+    std::cout << "\tLast modification: " << std::put_time(localtime, "%c")
+              << std::endl;
+
+    // Info #7: As you may be wondering by this time, what about permissions? 
+    // Well you can use the member 'perms' with its submemebrs to check which
+    // are active for the owner, group or others. For this we use a lambda.
+    auto ls_al_file = [](my_machine::perms perms)
+    {
+        using my_perm = my_machine::perms;
+        auto ow_r = ((perms & my_perm::owner_read) != my_perm::none ?
+            "r" : "-");
+        auto ow_w = ((perms & my_perm::owner_write) != my_perm::none ?
+            "w" : "-");
+        auto ow_x = ((perms & my_perm::owner_exec) != my_perm::none ?
+            "x" : "-");
+        auto gr_r = ((perms & my_perm::group_read) != my_perm::none ?
+            "r" : "-");
+        auto gr_w = ((perms & my_perm::group_write) != my_perm::none ?
+            "w" : "-");
+        auto gr_x = ((perms & my_perm::group_exec) != my_perm::none ?
+            "x" : "-");
+        auto ot_r = ((perms & my_perm::others_read) != my_perm::none ?
+            "r" : "-");
+        auto ot_w = ((perms & my_perm::others_write) != my_perm::none ?
+            "w" : "-");
+        auto ot_x = ((perms & my_perm::others_exec) != my_perm::none ?
+            "x" : "-");
+        std::cout << "\tPermissions: " << ow_r << ow_w << ow_x << " | " 
+                  << gr_r << gr_w << gr_x << " | " << ot_r << ot_w << ot_x
+                  << std::endl;
+    };
+    auto status = my_machine::status(file_path, err);
+    ls_al_file(status.permissions());
+
+    // Info #8: You can also check if the provided path is a file, a dir or a
+    // symlink with the booleans members functions presented below:
+    std::cout << std::boolalpha << "\tRegular file?: "
+              << my_machine::is_regular_file(file_path, err) << std::endl
+              << "\tDirectory?: "
+              << my_machine::is_directory(file_path, err) << std::endl
+              << "\tSymlink?: "
+              << my_machine::is_symlink(file_path, err) << std::endl;
+
     return 0;
 }
