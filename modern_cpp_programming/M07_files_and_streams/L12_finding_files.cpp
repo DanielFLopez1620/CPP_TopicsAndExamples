5eace55d4c662f460a7f3945deb9af229319ab95
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-01-18 20:27:27 -0500

    Modern C++ M07: Searching for a file by name coincidence or extension

--- modern_cpp_programming/M07_files_and_streams/L12_finding_files.cpp
+++ modern_cpp_programming/M07_files_and_streams/L12_finding_files.cpp
@@ -30,23 +30,72 @@
  * Check the function created based on the previous lesson to search
  * iteratively, and review the find files function too.
  * 
+ * To search for files or dirs we want, we require to iterate over the through
+ * the entries, then we can use the 'recursive_directory_iterator' in order to
+ * explore one path and search for a pattern (it can be validated using a 
+ * function), when doing this you should consider following the symlinks with
+ * the option 'follow_directory_symlink' in the 'filesystem::directory_option'.
+ * 
+ * When ready, compile and run the code with:
+ * 
+ *      g++ -std=c++17 L11_enum_content.cpp -o enum_content.out
+ *      ./enum_content.out
+ *      g++ -std=c++17 L12_finding_files.cpp -o search.out
+ *      ./search.out
  */
 
-#include <filesystem>
-#include <vector>
-#include <functional>
-#include <regex>
 
+// -------------------------- REQUIRED LIBRARIES ------------------------------
+#include <filesystem> // For using the machine's filesystem
+#include <vector>     // For dynamic arrays usage
+#include <functional> // Related with functions implementations
+
+// -------------------------- NAMESPACES CONSIDERED --------------------------
 namespace my_machine = std::filesystem;
 
+// --------------------------- FUNCTION PROTOTYPES ----------------------------
 std::vector<my_machine::path> search_file(
     my_machine::path const & dir,
     std::function<bool(my_machine::path const&)> filter);
 
+// --------------------------------- MAIN IMPLEMENTATION ----------------------
 int main(int argc, char* argv[])
 {
+    // Info #1: You can search for files that start or contains certain pattern
+    // by using a lambda with a str member function to find it.
+    std::cout << "Searching for files that start with 'info': " << std::endl;
+    auto founds = search_file(my_machine::current_path() / "example",
+        [](my_machine::path const & entry)
+        {
+            // Convert to wstring
+            auto filename = entry.wstring();
+
+            // Compare the find position and validate it isn't outside the size
+            return filename.find(L"info") != std::wstring::npos;
+        });
+    for (auto const & element : founds)
+    {
+        std::cout << "\t" << element << std::endl;
+    }
+
+    // Info #2: You can also search according extensions, by using the
+    // 'extension' member.
+    std::cout << "Searching for files that  have .txt ext': " << std::endl;
+    founds = search_file(my_machine::current_path() / "example",
+        [](my_machine::path const & entry)
+        {
+            return entry.extension() == L".txt";
+        });
+    for (auto const & element : founds)
+    {
+        std::cout << "\t" << element << std::endl;
+    }
+
     return 0;
-}
+
+} // main()
+
+// ------------------------------- FUNCTION DEFINITIONS -----------------------
 
 std::vector<my_machine::path> search_file(
     my_machine::path const & dir,
@@ -68,6 +117,6 @@ std::vector<my_machine::path> search_file(
             }
         }
     }
-
     return result;
-}
+
+} // search_file()
