b3e70950c81f7a67e58e409966d4c9af5ded2d87
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-04-05 09:31:28 -0500

    Modern C++ M08: Integrating fold expression, but pending execution

--- modern_cpp_programming/M08_threads_and_concurrency/L09_parallel_map.cpp
+++ modern_cpp_programming/M08_threads_and_concurrency/L09_parallel_map.cpp
@@ -17,10 +17,9 @@
 #include <numeric>
 #include <chrono>
 #include <cassert>
+#include <functional>
 
 const int THRESHOLD = 10000;
-
-unsigned get_no_of_threads();
 
 template <typename Time = std::chrono::microseconds,
     typename Clock = std::chrono::high_resolution_clock>
@@ -39,15 +38,121 @@ template <typename Time = std::chrono::microseconds,
     }
 };
 
+// ---------------------------- FUNCTION PROTOTYPES --------------------------
+
+unsigned get_no_of_threads();
+
+
 // Info #1: Let's create a parallel version of map, to start we create a
-// template that considers the 'begin' and 'end' iterators.
+// template that considers the 'begin' and 'end' iterators. Go to the function
+// definition and info #2 to understand the implementation.
+template <typename Iter, typename F>
+void parallel_map(Iter begin, Iter end, F f);
+
+// Info #3: Let's also create a parallel version of fold, here we also
+// consider a prototype that considers the 'begin' and 'end' iterator. But also,
+// the 'init' value and the 'operation/function' to implement.
+template <typename Iter, typename R, typename F>
+auto parallel_fold(Iter begin, Iter end, R init, F op);
+
+// --------------------------- MAIN IMPLEMENTATION ----------------------------
+
+int main(int argc, char* argv[])
+{
+    std::vector<int> sizes
+    {
+    10000, 100000, 500000, 
+    1000000, 2000000, 5000000, 
+    10000000, 25000000, 50000000
+    };
+
+    std::cout << std::right << std::setw(8) << std::setfill(' ') << "Size"
+              << std::right << std::setw(8) << std::setfill(' ') << "Std Map"
+              << std::right << std::setw(8) << std::setfill(' ') << "Par Map"
+              << std::endl;
+
+    for(auto const size : sizes)
+    {
+        std::vector<int> num_vec(size);
+        std::iota(std::begin(num_vec), std::end(num_vec), 1);
+        auto vec_copy1 = num_vec;
+        auto nor_map = perf_timer<>::duration([&]
+            {
+                std::transform(std::begin(vec_copy1), std::end(vec_copy1),
+                    std::begin(vec_copy1), [](int const i){return i * i; }); 
+            });
+        auto vec_copy2 = num_vec;
+        auto  pl_map = perf_timer<>::duration([&]
+            {
+                parallel_map(std::begin(vec_copy2), std::end(vec_copy2),
+                    [](int const i){return i * i; }); 
+            });
+        std::cout << std::right << std::setw(8) << std::setfill(' ') << size
+                  << std::right << std::setw(8) 
+                  << std::chrono::duration<double, std::micro>(nor_map).count()
+                  << std::right << std::setw(8) 
+                  << std::chrono::duration<double, std::micro>(pl_map).count()
+                  << std::endl;
+    }
+
+    std::cout << std::right << std::setw(8) << std::setfill(' ') << "Size"
+              << std::right << std::setw(8) << std::setfill(' ') << "Std Fld"
+              << std::right << std::setw(8) << std::setfill(' ') << "Par Fld"
+              << std::endl;
+
+    for(auto const size : sizes)
+    {
+        std::vector<int> num_vec2(size);
+        std::iota(std::begin(num_vec2), std::end(num_vec2), 1);
+        auto vec_copy3 = num_vec2;
+        auto nor_fld = perf_timer<>::duration([&]
+            {
+                std::accumulate(std::begin(vec_copy3), std::end(vec_copy3),
+                    0LL, std::plus<>()); 
+            });
+        auto vec_copy4 = num_vec2;
+        auto pl_fld = perf_timer<>::duration([&]
+            {
+                parallel_fold(std::begin(vec_copy4), std::end(vec_copy4),
+                    0LL, std::plus<>()); 
+            });
+        std::cout << std::right << std::setw(8) << std::setfill(' ') << size
+                  << std::right << std::setw(8) 
+                  << std::chrono::duration<double, std::micro>(nor_fld).count()
+                  << std::right << std::setw(8) 
+                  << std::chrono::duration<double, std::micro>(pl_fld).count()
+                  << std::endl;
+    }
+
+    return 0;
+}
+
+// ------------------------ FUNCTION DEFINITIONS ------------------------------
+
+/**
+ * Return the number allowed of threads for concurrency
+ * 
+ * @return Hardware concurrency
+ */
+unsigned get_no_of_threads()
+{
+    return std::thread::hardware_concurrency();
+}
+
+/**
+ * Implement a map that is paralel in case of a big number of features to process.
+ * 
+ * @param begin Beginning iterator of the array
+ * @param end End iterator of the array
+ * @param f Function to implement to each element
+ */
 template <typename Iter, typename F>
 void parallel_map(Iter begin, Iter end, F f)
 {
     // Info #2: Then check the size of the range and compare it with a
     // threshold, so if it isn0t surpasssed it do the sequential exec.
     auto len = std::distance(begin, end);
-    if(size <= THRESHOLD)
+    if(len <= THRESHOLD)
     {
         std::transform(begin, end, begin, std::forward<F>(f));
     }
@@ -77,17 +182,49 @@ void parallel_map(Iter begin, Iter end, F f)
         {
             th.join();
         }
-
     }
 }
 
-
-int main(int argc, char* argv[])
+template <typename Iter, typename R, typename F>
+auto parallel_fold(Iter begin, Iter end, R init, F op)
 {
-    return 0;
-}
+    auto size = std::distance(begin, end);
+    if(size <= 10000)
+    {
+        return std::accumulate(begin, end, init, std::forward<F>(op));
+    }
+    else
+    {
+        auto no_of_th = get_no_of_threads();
+        auto part = size / no_of_th;
+        auto last = begin;
 
-unsigned get_no_of_threads()
-{
-    return std::thread::hardware_concurrency();
+        std::vector<std::thread> ths;
+        std::vector<R> values (no_of_th);
+        for(unsigned i = 0; i < no_of_th; ++i)
+        {
+            if(i == no_of_th - 1)
+            {
+                last = end;
+            }
+            else
+            {
+                std::advance(last, part);
+            }
+            ths.emplace_back(
+                [=, &op](R& result)
+                {
+                    result = std::accumulate(begin, last, R{},
+                        std::forward<F>(op));
+                }, std::ref(values[i])
+            );
+            begin = last;
+        }
+        for(auto & th : ths)
+        {
+            th.join();
+        }
+        return std::accumulate(std::begin(values), std::end(values), init,
+            std::forward<F>(op));
+    }
 }
